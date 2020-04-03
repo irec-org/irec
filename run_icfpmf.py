7fd2dd4f221a0201c88106c2b5945c4a41f0e6b1
heitor57 <heitorwerneck@hotmail.com>
2020-04-03 20:29:47 -0300

    run icfpmf

---
+++ run_icfpmf.py
@@ -0,0 +1,13 @@
+import inquirer
+import numpy as np
+
+import interactors
+from mf import ICFPMF
+from util import DatasetFormatter
+
+dsf = DatasetFormatter()
+dsf = dsf.load()
+
+mf = ICFPMF()
+mf.load_var(dsf.matrix_users_ratings[dsf.train_uids])
+mf.fit(dsf.matrix_users_ratings[dsf.train_uids])
