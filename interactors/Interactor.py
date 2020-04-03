587fdd2fd11f30c3d20858d02868188cd945f3d6
heitor57 <heitorwerneck@hotmail.com>
2020-04-03 01:18:18 -0300

    .pickle and results

--- interactors/Interactor.py
+++ interactors/Interactor.py
@@ -2,11 +2,15 @@ import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 
 from util import Nameable
+from util import DirectoryDependent
+from collections import defaultdict
+import pickle
 
-class Interactor(Nameable):
+class Interactor(Nameable, DirectoryDependent):
     def __init__(self, consumption_matrix=None, interactions=20):
         self.consumption_matrix = consumption_matrix
         self.interactions = interactions
+        self.result = defaultdict(list)
         pass
 
     def get_reward(self,uid,iid):
@@ -15,3 +19,12 @@ class Interactor(Nameable):
     def interact(self):
         print(self.get_verbose_name())
         pass
+
+    def save_result(self):
+        with open(f'{os.path.join(self.DIRS["result"],self.get_name())}.pickle', "wb") as f:
+            pickle.dump(self.result, f)
+
+    def load_result(self):
+        with open(f'{os.path.join(self.DIRS["result"],self.get_name())}.pickle', "rb") as f:
+            return pickle.load(f)
+
