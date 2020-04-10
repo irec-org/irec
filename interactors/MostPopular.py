e13a96b9b37afa1a6b48078b12c143197bbcc2f5
heitor57 <heitorwerneck@hotmail.com>
2020-04-10 21:41:49 +0000

    most popular function

--- interactors/MostPopular.py
+++ interactors/MostPopular.py
@@ -5,13 +5,20 @@ class MostPopular(Interactor):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @staticmethod
+    def get_most_popular(consumption_matrix,test_uids):
+        uids = test_uids
+        num_users = len(uids)
+        mask = np.ones(consumption_matrix.shape[0], dtype=bool)
+        mask[uids] = 0
+        lowest_value = np.min(consumption_matrix)
+        top_iids = list(reversed(np.argsort(np.count_nonzero(consumption_matrix[mask,:]>lowest_value,axis=0))))
+        return top_iids
+
     def interact(self, uids):
         super().interact()
+        top_iids = self.get_most_popular(self.consumption_matrix, uids)[:self.get_iterations()]
         num_users = len(uids)
-        mask = np.ones(self.consumption_matrix.shape[0], dtype=bool)
-        mask[uids] = 0
-        lowest_value = np.min(self.consumption_matrix)
-        top_iids = list(reversed(np.argsort(np.count_nonzero(self.consumption_matrix[mask,:]>lowest_value,axis=0))))[:self.interactions*self.interaction_size]
         for idx_uid in tqdm(range(num_users)):
             uid = uids[idx_uid]
             self.result[uid].extend(top_iids)
