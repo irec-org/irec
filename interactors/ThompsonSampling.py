6a7d891200c1a84f0e9a90465eb36c21c8b49224
heitor57 <heitorwerneck@hotmail.com>
2020-05-02 21:11:48 -0300

    thompson random user evaluation

--- interactors/ThompsonSampling.py
+++ interactors/ThompsonSampling.py
@@ -4,6 +4,7 @@ from . import Interactor
 import os
 import random
 import scipy.stats
+from collections import defaultdict
 class ThompsonSampling(Interactor):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,22 +19,29 @@ class ThompsonSampling(Interactor):
         self.alphas = np.ones(num_items)
         self.betas = np.ones(num_items)
 
-        for i in tqdm(range(self.get_iterations())):
-            for idx_uid in range(num_users):
-                uid = uids[idx_uid]
-                # best_item = np.argmax([self.beta_distributions[item].rvs() for item in range(num_items)])
-                not_recommended = np.ones(num_items,dtype=bool)
-                not_recommended[self.result[uid]] = 0
-                items_not_recommended = np.nonzero(not_recommended)[0]
-                best_item = items_not_recommended[np.argmax(np.random.beta(self.alphas[items_not_recommended],
-                                                     self.betas[items_not_recommended]))]
-                # reward = (self.get_reward(uid,best_item)-self.lowest_value)/(self.highest_value-self.lowest_value)
-                # reward = 1 if reward >= 0.8 else 0
+        users_num_interactions = defaultdict(int)
+        available_users = set(uids)
+
+        for i in tqdm(range(num_users*self.interactions)):
+            uid = random.sample(available_users,k=1)[0]
+            users_num_interactions[uid] += 1
+            if users_num_interactions[uid] == self.interactions:
+                available_users = available_users - {uid}
+
+            # best_item = np.argmax([self.beta_distributions[item].rvs() for item in range(num_items)])
+            not_recommended = np.ones(num_items,dtype=bool)
+            not_recommended[self.result[uid]] = 0
+            items_not_recommended = np.nonzero(not_recommended)[0]
+            items_score = np.random.beta(self.alphas[items_not_recommended],
+                                         self.betas[items_not_recommended])
+            top_items = list(reversed(np.argsort(items_score)))[:self.interaction_size]
+            best_items = items_not_recommended[top_items]
+            # reward = (self.get_reward(uid,best_item)-self.lowest_value)/(self.highest_value-self.lowest_value)
+            # reward = 1 if reward >= 0.8 else 0
+            for best_item in best_items:
                 reward = self.get_reward(uid,best_item)
                 reward = 1 if reward >= self.values[-2] else 0
-                
                 self.alphas[best_item] += reward
                 self.betas[best_item] += 1-reward
-                # self.betas[best_item] += self.highest_value-reward
                 self.result[uid].append(best_item)
         self.save_result()
