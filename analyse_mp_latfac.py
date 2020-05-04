2255d590b55a4941ebdbab08f6ad9126b2350971
heitor57 <heitorwerneck@hotmail.com>
2020-05-04 19:28:03 +0000

    analyse most popular and mf correlations

---
+++ analyse_mp_latfac.py
@@ -0,0 +1,36 @@
+import inquirer
+import interactors
+from mf import ICFPMF
+from util import DatasetFormatter
+from sklearn.decomposition import NMF
+import numpy as np
+import matplotlib.pyplot as plt
+
+dsf = DatasetFormatter()
+dsf = dsf.load()
+model = NMF(n_components=10, init='nndsvd', random_state=0)
+P = model.fit_transform(dsf.matrix_users_ratings[dsf.train_uids])
+Q = model.components_.T
+
+items_popularity = interactors.MostPopular.get_items_popularity(dsf.matrix_users_ratings,dsf.test_uids)
+
+top_iids_mp = list(reversed(np.argsort(items_popularity)))
+
+num_items = 20
+
+
+top_iids_mf = list(reversed(np.argsort(np.sum(Q,axis=1))))
+
+plt.hist(np.sum(Q,axis=1))
+
+plt.savefig('a.png')
+
+print('Item MP\tItem MF\tSUM\tMEAN\tMEDIAN\tMAX\tMIN\tSTD')
+for i in range(num_items):
+    item_mf = top_iids_mf[i]
+    item_mp = top_iids_mp[i]
+    print(item_mp,item_mf,"%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f"%(np.sum(Q[item_mf]),
+          np.mean(Q[item_mf]),np.median(Q[item_mf]),np.max(Q[item_mf]),
+                                                           np.min(Q[item_mf]),np.std(Q[item_mf])),sep='\t')
+    print([f'{i:.2f}' for i in list(Q[item_mf])])
+    
