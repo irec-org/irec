00a6e8ee561aa3c493ccf30798b7f06d4afa131e
heitor57 <heitorwerneck@hotmail.com>
2020-05-11 19:53:54 +0000

    NMF and SVD

---
+++ mf/NMF.py
@@ -0,0 +1,8 @@
+from . import MF
+import sklearn.decomposition
+class NMF(MF):
+    def fit(self,training_matrix):
+        model = sklearn.decomposition.NMF(n_components=self.num_lat, init='nndsvd', random_state=0)
+        self.users_weights = model.fit_transform(training_matrix)
+        self.items_weights = model.components_.T
+        self.save()
---
+++ mf/SVD.py
@@ -0,0 +1,10 @@
+from . import MF
+import scipy.sparse
+class SVD(MF):
+    def fit(self,training_matrix):
+        u, s, vt = scipy.sparse.linalg.svds(
+            scipy.sparse.csr_matrix(training_matrix),
+            k=self.num_lat)
+        self.users_weights = u
+        self.items_weights = vt.T
+        self.save()
