f3a71409a13f79ce5405fbc73528d18151c1e8b9
heitor57 <heitorwerneck@hotmail.com>
2020-05-11 03:49:06 +0000

    get items representativeness

--- interactors/MostRepresentative.py
+++ interactors/MostRepresentative.py
@@ -7,10 +7,13 @@ class MostRepresentative(Interactor):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @staticmethod
+    def get_items_representativeness(items_latent_factors):
+        return np.sum(items_latent_factors**2,axis=1)
+
     def interact(self, uids, items_latent_factors):
         super().interact()
-        items_representativeness = np.sum(items_latent_factors**2,axis=1)
-        
+        items_representativeness = self.get_items_representativeness(items_latent_factors)
         top_iids = list(reversed(np.argsort(items_representativeness)))[:self.get_iterations()]
         num_users = len(uids)
         for idx_uid in tqdm(range(num_users)):
