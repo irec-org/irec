c1b60b00141f3ce7ea5a671f885eb34842062fa8
heitor57 <heitorwerneck@hotmail.com>
2021-01-04 18:12:56 -0300

    csr_matrix fix construction

--- src/lib/interactors/ALMostPopular.py
+++ src/lib/interactors/ALMostPopular.py
@@ -14,7 +14,7 @@ class ALMostPopular(Interactor):
     def train(self,train_dataset):
         super().train(train_dataset)
         self.train_dataset = train_dataset
-        self.train_consumption_matrix = scipy.sparse.csr_matrix((train_data[2],(train_data[0],train_data[1])),(self.train_dataset.users_num,self.train_dataset.items_num))
+        self.train_consumption_matrix = scipy.sparse.csr_matrix((self.train_dataset.data[2],(self.train_dataset.data[0],self.train_dataset.data[1])),(self.train_dataset.users_num,self.train_dataset.items_num))
         self.num_items = self.train_consumption_matrix.shape[1]
         self.items_popularity = MostPopular.get_items_popularity(self.train_consumption_matrix, normalize=False)
 
