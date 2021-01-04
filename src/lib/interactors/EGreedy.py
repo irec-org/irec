f1405f22c8f75a1f85c521e0f35d98ea7d928189
heitor57 <heitorwerneck@hotmail.com>
2021-01-04 18:08:54 -0300

    egreedy refactor

--- src/lib/interactors/EGreedy.py
+++ src/lib/interactors/EGreedy.py
@@ -10,43 +10,41 @@ class EGreedy(Interactor):
         super().__init__(*args, **kwargs)
         self.epsilon = epsilon
 
-    def interact(self):
-        super().interact()
-        uids = self.test_users
-        num_users = len(uids)
-        num_items = self.train_consumption_matrix.shape[1]
+    def train(self,train_dataset):
+        super().train(train_dataset)
+        self.train_dataset = train_dataset
+        self.train_consumption_matrix = scipy.sparse.csr_matrix((train_data[2],(train_data[0],train_data[1])),(self.train_dataset.users_num,self.train_dataset.items_num))
+        self.num_items = self.train_consumption_matrix.shape[1]
+        # uids = self.test_users
+        # num_users = len(uids)
+        # num_items = self.train_consumption_matrix.shape[1]
         
-        items_mean_values = np.zeros(num_items)
-        items_count = np.zeros(num_items,dtype=int)
+        self.items_mean_values = np.zeros(self.num_items)
+        self.items_count = np.zeros(self.num_items,dtype=int)
 
-        users_num_interactions = defaultdict(int)
-        available_users = set(uids)
+        # users_num_interactions = defaultdict(int)
+        # available_users = set(uids)
 
-        mask = np.ones(self.train_consumption_matrix.shape[0], dtype=bool)
-        mask[uids] = 0
-        items_mean_values = np.mean(self.train_consumption_matrix[mask],axis=0).A.flatten()
-        items_count += self.train_consumption_matrix[mask].shape[0]
+        # mask = np.ones(self.train_consumption_matrix.shape[0], dtype=bool)
+        # mask[uids] = 0
+        self.items_mean_values = np.mean(self.train_consumption_matrix[self.train_dataset.uids],axis=0).A.flatten()
+        self.items_count += self.train_consumption_matrix[self.train_dataset.uids].shape[0]
 
-        for i in tqdm(range(num_users*self.interactions)):
-            uid = random.sample(available_users,k=1)[0]
+        # for i in tqdm(range(num_users*self.interactions)):
+        #     uid = random.sample(available_users,k=1)[0]
 
-            for j in range(self.interaction_size):
-                not_recommended = np.ones(num_items,dtype=bool)
-                not_recommended[self.results[uid]] = 0
-                items_not_recommended = np.nonzero(not_recommended)[0]
-                if self.epsilon < np.random.rand():
-                    best_item = items_not_recommended[np.argmax(items_mean_values[items_not_recommended])]
-                else:
-                    best_item = random.choice(items_not_recommended)
-                self.results[uid].append(best_item)
-
-            user_num_interactions = users_num_interactions[uid]
-            for best_item in self.results[uid][user_num_interactions*self.interaction_size:(user_num_interactions+1)*self.interaction_size]:
-                items_mean_values[best_item] = (items_mean_values[best_item]*items_count[best_item]+self.get_reward(uid,best_item))/(items_count[best_item] + 1)
-                items_count[best_item] += 1
-
-            users_num_interactions[uid] += 1
-            if users_num_interactions[uid] == self.interactions:
-                available_users = available_users - {uid}
-
-        self.save_results()
+            # for j in range(self.interaction_size):
+    def predict(self,uid,candidate_items):
+                # not_recommended = np.ones(num_items,dtype=bool)
+                # not_recommended[self.results[uid]] = 0
+                # items_not_recommended = np.nonzero(not_recommended)[0]
+        if self.epsilon < np.random.rand():
+            items_score = items_mean_values[candidate_items]
+        else:
+            items_score = np.random.rand(len(candidate_items))
+            # best_item = random.choice(items_not_recommended)
+        return items_score, None
+                
+    def update(self,uid,item,reward,additional_data):
+        self.items_mean_values[item] = (self.items_mean_values[item]*self.items_count[item]+reward)/(self.items_count[item] + 1)
+        self.items_count[item] += 1
