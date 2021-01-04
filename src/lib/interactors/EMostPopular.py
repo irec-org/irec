604bb804b24cbea2dc71ec070181e4e6b31b183e
heitor57 <heitorwerneck@hotmail.com>
2021-01-04 18:23:23 -0300

    emostpopular refactor

--- src/lib/interactors/EMostPopular.py
+++ src/lib/interactors/EMostPopular.py
@@ -31,12 +31,23 @@ class EMostPopular(Interactor):
     def interact(self):
         super().interact()
         uids = self.test_users
-        items_entropy = Entropy.get_items_entropy(self.train_consumption_matrix)
+        self.items_entropy = Entropy.get_items_entropy(self.train_consumption_matrix)
         # items_entropy0 = Entropy0.get_items_entropy(self.train_consumption_matrix)
-        items_popularity = MostPopular.get_items_popularity(self.train_consumption_matrix,normalize=False)
+        self.items_popularity = MostPopular.get_items_popularity(self.train_consumption_matrix,normalize=False)
 
         # items_probability = items_entropy/items_entropy.sum()
-        top_iids = self.top_emostpopular(items_popularity,items_entropy)
+        self.top_iids = self.top_emostpopular(items_popularity,items_entropy)
+        self.items_score = np.empty(len(self.top_iids))
+        for i, iid in enumerate(reversed(self.top_iids)):
+            self.items_score[iid] = i
+            
+
+    def predict(self,uid,candidate_items):
+        items_score = self.items_score[candidate_items]
+        return items_score, None
+
+    def update(self,uid,item,reward,additional_data):
+        pass
         # top_iids = []
         # num_items = self.train_consumption_matrix.shape[1]
         # for i in range(num_items):
@@ -57,28 +68,28 @@ class EMostPopular(Interactor):
         # top_popularity_iids = list(reversed(np.argsort(items_entropy)))[:self.get_iterations()]
         # top_entropy_iids = list(reversed(np.argsort(items_popularity)))[:self.get_iterations()]
 
-        correlation = scipy.stats.pearsonr(items_entropy,items_popularity)[0]
+        # correlation = scipy.stats.pearsonr(items_entropy,items_popularity)[0]
 
-        # top_iids = list(reversed(np.argsort(items_popplusent)))[:self.get_iterations()]
+        # # top_iids = list(reversed(np.argsort(items_popplusent)))[:self.get_iterations()]
 
-        fig, ax = plt.subplots()
-        ax.scatter(items_entropy,items_popularity,marker="D",color='darkblue')
-        ax.set_ylabel("Popularity")
-        ax.set_xlabel("Entropy")
-        ax.text(0.3, 0.9 , f'Correlation coefficient: {correlation:.2f}', color='k',
-                ha='center', va='center',
-                bbox=dict(facecolor='none', edgecolor='k', pad=10.0),
-                transform = ax.transAxes)
-        for start, end, color in [(0,10,'green'),(10,20,'red'),(20,30,'darkred'),(30,40,'yellow'),(40,50,'orange')]:
-            ax.scatter(items_entropy[top_iids[start:end]],items_popularity[top_iids[start:end]],marker='D',color=color)
-            print("[%d,%d] sum(popularity)=%.2f sum(entropy)=%.2f"%(start,end,
-                                                                 np.sum(items_popularity[top_iids[start:end]]/np.max(items_popularity)),
-                                                                 np.sum(items_entropy[top_iids[start:end]]/np.max(items_entropy))))
-        fig.savefig(os.path.join(self.DIRS['img'],"corr_popent_"+self.get_name()+".png"))
+        # fig, ax = plt.subplots()
+        # ax.scatter(items_entropy,items_popularity,marker="D",color='darkblue')
+        # ax.set_ylabel("Popularity")
+        # ax.set_xlabel("Entropy")
+        # ax.text(0.3, 0.9 , f'Correlation coefficient: {correlation:.2f}', color='k',
+        #         ha='center', va='center',
+        #         bbox=dict(facecolor='none', edgecolor='k', pad=10.0),
+        #         transform = ax.transAxes)
+        # for start, end, color in [(0,10,'green'),(10,20,'red'),(20,30,'darkred'),(30,40,'yellow'),(40,50,'orange')]:
+        #     ax.scatter(items_entropy[top_iids[start:end]],items_popularity[top_iids[start:end]],marker='D',color=color)
+        #     print("[%d,%d] sum(popularity)=%.2f sum(entropy)=%.2f"%(start,end,
+        #                                                          np.sum(items_popularity[top_iids[start:end]]/np.max(items_popularity)),
+        #                                                          np.sum(items_entropy[top_iids[start:end]]/np.max(items_entropy))))
+        # fig.savefig(os.path.join(self.DIRS['img'],"corr_popent_"+self.get_name()+".png"))
 
 
-        num_users = len(uids)
-        for idx_uid in tqdm(range(num_users)):
-            uid = uids[idx_uid]
-            self.results[uid].extend(top_iids)
-        self.save_results()
+        # num_users = len(uids)
+        # for idx_uid in tqdm(range(num_users)):
+        #     uid = uids[idx_uid]
+        #     self.results[uid].extend(top_iids)
+        # self.save_results()
