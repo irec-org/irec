c45ec0a3377eac00a62c9fbacf8d6fc37ce619ac
heitor57 <heitorwerneck@hotmail.com>
2021-01-04 21:52:04 -0300

    HELF

--- src/lib/interactors/HELF.py
+++ src/lib/interactors/HELF.py
@@ -23,28 +23,37 @@ class HELF(Interactor):
         items_helf[np.isnan(items_helf)] = 0
         return items_helf
 
-    def interact(self):
-        super().interact()
-        uids = self.test_users
+    def train(self,train_dataset):
+        super().train(train_dataset)
+        self.train_dataset = train_dataset
+        self.train_consumption_matrix = scipy.sparse.csr_matrix((self.train_dataset.data[2],(self.train_dataset.data[0],self.train_dataset.data[1])),(self.train_dataset.users_num,self.train_dataset.items_num))
+        self.num_items = self.train_dataset.num_items
         # mask = np.ones(self.train_consumption_matrix.shape[0], dtype=bool)
         # mask[uids] = 0
-        num_train_users = len(self.train_users)
+        num_train_users = len(self.train_dataset.uids)
         items_entropy = Entropy.get_items_entropy(self.train_consumption_matrix)
         items_popularity = MostPopular.get_items_popularity(self.train_consumption_matrix,normalize=False)
-        items_logpopent = HELF.get_items_helf(items_popularity,items_entropy,num_train_users)
-        top_iids = list(reversed(np.argsort(items_logpopent)))[:self.get_iterations()]
-        num_users = len(uids)
+        self.items_logpopent = HELF.get_items_helf(items_popularity,items_entropy,num_train_users)
 
 
-        fig, ax = plt.subplots()
-        ax.scatter(items_entropy,items_popularity,marker="D",color='darkblue')
-        ax.set_ylabel("Popularity")
-        ax.set_xlabel("Entropy")
-        for start, end, color in [(0,10,'green'),(10,20,'red'),(20,30,'darkred'),(30,40,'yellow'),(40,50,'orange')]:
-            ax.scatter(items_entropy[top_iids[start:end]],items_popularity[top_iids[start:end]],marker='D',color=color)
-        fig.savefig(os.path.join(self.DIRS['img'],"corr_popent_"+self.get_name()+".png"))
+    def predict(self,uid,candidate_items):
+        items_score = self.items_logpopent[candidate_items]
+        return items_score, None
+        # top_items = list(reversed(np.argsort(items_score)))[:self.interaction_size]
 
-        for idx_uid in tqdm(range(num_users)):
-            uid = uids[idx_uid]
-            self.results[uid].extend(top_iids)
-        self.save_results()
+        # top_iids = list(reversed(np.argsort(items_logpopent)))[:self.get_iterations()]
+        # num_users = len(uids)
+
+
+        # fig, ax = plt.subplots()
+        # ax.scatter(items_entropy,items_popularity,marker="D",color='darkblue')
+        # ax.set_ylabel("Popularity")
+        # ax.set_xlabel("Entropy")
+        # for start, end, color in [(0,10,'green'),(10,20,'red'),(20,30,'darkred'),(30,40,'yellow'),(40,50,'orange')]:
+        #     ax.scatter(items_entropy[top_iids[start:end]],items_popularity[top_iids[start:end]],marker='D',color=color)
+        # fig.savefig(os.path.join(self.DIRS['img'],"corr_popent_"+self.get_name()+".png"))
+
+        # for idx_uid in tqdm(range(num_users)):
+        #     uid = uids[idx_uid]
+        #     self.results[uid].extend(top_iids)
+        # self.save_results()
