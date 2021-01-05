a99357774cfbe60e9f630ef2ece24322fcc6190b
heitor57 <heitorwerneck@hotmail.com>
2021-01-04 21:41:30 -0300

    glm ucb refactor

--- src/lib/interactors/GLM_UCB.py
+++ src/lib/interactors/GLM_UCB.py
@@ -16,56 +16,65 @@ class GLM_UCB(ICF):
     def p(self,x):
         return x
 
-    def interact(self, items_means):
-        super().interact()
-        uids = self.test_users
-        self.items_means = items_means
-        num_users = len(uids)
-        # get number of latent factors 
-        self_id = id(self)
-        with threadpool_limits(limits=1, user_api='blas'):
-            args = [(self_id,int(uid),) for uid in uids]
-            results = util.run_parallel(self.interact_user,args)
-        for i, user_result in enumerate(results):
-            self.results[uids[i]] = user_result
-        self.save_results()
+    def train(self,train_dataset):
+        super().train(train_dataset)
+        self.train_dataset = train_dataset
+        self.train_consumption_matrix = scipy.sparse.csr_matrix((self.train_dataset.data[2],(self.train_dataset.data[0],self.train_dataset.data[1])),(self.train_dataset.users_num,self.train_dataset.items_num))
+        self.num_items = self.train_dataset.num_items
+
+        # self.items_means = items_means
+
+        mf_model = mf.ICFPMFS()
+        mf_model.fit(self.train_consumption_matrix)
+        self.items_means = mf_model.items_means
+        self.num_latent_factors = len(self.items_latent_factors[0])
+        self.I = np.eye(self.num_latent_factors)
+        A = self.user_lambda*I
+        self.As = defaultdict(lambda: np.copy(A))
+        self.users_rec_rewards = defaultdict(list)
+        self.users_rec_items_means = defaultdict(list)
 
     def error_user_weight_function(self,p,u_rec_rewards,u_rec_items_means):
         return np.sum(np.array(
             [(u_rec_rewards[t] - self.p(p.T @ u_rec_items_means[t]))*u_rec_items_means[t]
              for t in range(0,len(u_rec_items_means))]),0)
 
-    @staticmethod
-    def interact_user(obj_id,uid):
-        self = ctypes.cast(obj_id, ctypes.py_object).value
-        if not issubclass(self.__class__,ICF): # DANGER CODE
-            raise RuntimeError
-        num_lat = len(self.items_means[0])
-        I = np.eye(num_lat)
+    def predict(self,uid,candidate_items):
+        # self = ctypes.cast(obj_id, ctypes.py_object).value
+        # if not issubclass(self.__class__,ICF): # DANGER CODE
+        #     raise RuntimeError
+        # num_lat = len(self.items_means[0])
+        
 
-        user_candidate_items = list(range(len(self.items_means)))
-        u_rec_rewards = []
-        u_rec_items_means = []
-        A = self.user_lambda*I
-        result = []
-        for i in range(self.interactions):
-            if len(u_rec_items_means) == 0:
-                p = np.zeros(num_lat)
-            else:
-                p = scipy.optimize.root(self.error_user_weight_function,
-                                        p,
-                                        (u_rec_rewards,u_rec_items_means)).x
-            cov = np.linalg.inv(A)*self.var
-            for j in range(self.interaction_size):
-                max_i = user_candidate_items[np.argmax(self.p(p[None,:] @ self.items_means[user_candidate_items].T) + self.c * np.sqrt(np.log(i*self.interaction_size+j+1)) *\
-                    np.sqrt(np.sum(self.items_means[user_candidate_items].dot(cov) * self.items_means[user_candidate_items],axis=1)))]
+        # user_candidate_items = list(range(len(self.items_means)))
+        # # u_rec_rewards = []
+        # # u_rec_items_means = []
+        self.users_rec_rewards[uid]
+        self.users_rec_items_means[uid]
+        A = self.As[uid]
+        # result = []
+        # for i in range(self.interactions):
+        if len(users_rec_items_means[uid]) == 0:
+            p = np.zeros(num_lat)
+        else:
+            p = scipy.optimize.root(self.error_user_weight_function,
+                                    p,
+                                    (users_rec_rewards[uid],users_rec_items_means[uid])).x
+        cov = np.linalg.inv(A)*self.var
+        # for j in range(self.interaction_size):
+        items_score = self.p(p[None,:] @ self.items_means[candidate_items].T) +\
+            self.c * np.sqrt(np.log(self.t+1)) *\
+            np.sqrt(np.sum(self.items_means[candidate_items].dot(cov) *\
+                           self.items_means[candidate_items],axis=1))
+        
+        return items_score, None
+        # user_candidate_items.remove(max_i)
+        # result.append(max_i)
 
-                user_candidate_items.remove(max_i)
-                result.append(max_i)
-
-            for max_i in result[i*self.interaction_size:(i+1)*self.interaction_size]:
-                max_item_mean = self.items_means[max_i]
-                u_rec_rewards.append(self.get_reward(uid,max_i))
-                u_rec_items_means.append(max_item_mean)
-                A += max_item_mean[:,None].dot(max_item_mean[None,:])
-        return result
+        # for max_i in result[i*self.interaction_size:(i+1)*self.interaction_size]:
+    def update(self,uid,item,reward,additional_data):
+        max_item_mean = self.items_means[item]
+        users_rec_rewards[uid].append(reward)
+        users_rec_items_means[uid].append(item)
+        self.As[uid] += max_item_mean[:,None].dot(max_item_mean[None,:])
+        # return result
