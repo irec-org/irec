314070c33f07f4ed486ef7ef704bbadc8fdab4b9
heitor57 <heitorwerneck@hotmail.com>
2021-01-06 03:50:25 -0300

    UCB refactor

--- src/lib/interactors/UCB.py
+++ src/lib/interactors/UCB.py
@@ -10,54 +10,36 @@ class UCB(ExperimentalInteractor):
         super().__init__(*args, **kwargs)
         self.c = c
 
-    def interact(self):
-        super().interact()
-        uids = self.test_users
-        np.seterr(divide='warn')
-        num_users = len(uids)
-        num_items = self.train_consumption_matrix.shape[1]
+    def train(self,train_dataset):
+        super().train(train_dataset)
+        self.train_dataset = train_dataset
+        self.train_consumption_matrix = scipy.sparse.csr_matrix((self.train_dataset.data[2],(self.train_dataset.data[0],self.train_dataset.data[1])),(self.train_dataset.users_num,self.train_dataset.items_num))
+        self.num_items = self.train_dataset.num_items
+        # uids = self.test_users
+        # np.seterr(divide='warn')
+        # num_users = len(uids)
+        # num_items = self.train_consumption_matrix.shape[1]
         
-        items_mean_values = np.zeros(num_items,dtype=np.float128)
-        items_count = np.zeros(num_items,dtype=int)
+        self.items_mean_values = np.zeros(self.num_items,dtype=np.float128)
+        self.items_count = np.zeros(self.num_items,dtype=int)
 
-        ctime = 1
+        self.t = 1
 
         users_num_interactions = defaultdict(int)
         available_users = set(uids)
 
         mask = np.ones(self.train_consumption_matrix.shape[0], dtype=bool)
         mask[uids] = 0
-        items_mean_values = np.mean(self.train_consumption_matrix[mask],axis=0).A.flatten()
-        items_count += self.train_consumption_matrix[mask].shape[0]
-        ctime += np.prod(self.train_consumption_matrix[mask].shape)
-
-        for i in tqdm(range(num_users*self.interactions)):
-            uid = random.sample(available_users,k=1)[0]
-
-            not_recommended = np.ones(num_items,dtype=bool)
-            not_recommended[self.results[uid]] = 0
-            items_not_recommended = np.nonzero(not_recommended)[0]
-
-            items_uncertainty = self.c*np.sqrt(2*np.log(ctime)/items_count[items_not_recommended])
-            items_score = items_mean_values[items_not_recommended]+items_uncertainty
-
-            # if ctime % 5000 <= 5:
-            #     print(np.max(items_uncertainty))
-            #     print(np.max(items_mean_values[items_not_recommended]))
-            top_items = list(reversed(np.argsort(items_score)))[:self.interaction_size]
-            best_items = items_not_recommended[top_items]
-            self.results[uid].extend(best_items)
-
-            user_num_interactions = users_num_interactions[uid]
-            for best_item in self.results[uid][user_num_interactions*self.interaction_size:(user_num_interactions+1)*self.interaction_size]:
-                items_mean_values[best_item] = (items_mean_values[best_item]*items_count[best_item]+self.get_reward(uid,best_item))/(items_count[best_item] + 1)
-                items_count[best_item] += 1
+        self.items_mean_values = np.mean(self.train_consumption_matrix[mask],axis=0).A.flatten()
+        self.items_count += self.train_consumption_matrix[mask].shape[0]
+        self.t += np.prod(self.train_consumption_matrix[mask].shape)
 
 
-            users_num_interactions[uid] += 1
-            if users_num_interactions[uid] == self.interactions:
-                available_users = available_users - {uid}
+    def predict(self,uid,candidate_items,num_req_items):
+        items_uncertainty = self.c*np.sqrt(2*np.log(self.t)/self.items_count[candidate_items])
+        items_score = self.items_mean_values[candidate_items]+items_uncertainty
+        return items_score, None
 
-            ctime += self.interaction_size
-
-        self.save_results()
+    def update(self,uid,item,reward,additional_data):
+        self.items_mean_values[best_item] = (self.items_mean_values[best_item]*self.items_count[best_item]+self.get_reward(uid,best_item))/(self.items_count[best_item] + 1)
+        self.items_count[best_item] += 1
