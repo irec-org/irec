61b68be0032beeeb2d2d61352f76bae13a4a50b9
heitor57 <heitorwerneck@hotmail.com>
2021-01-06 03:45:15 -0300

    random refactor

--- src/lib/interactors/Random.py
+++ src/lib/interactors/Random.py
@@ -6,14 +6,17 @@ class Random(ExperimentalInteractor):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def interact(self):
-        super().interact()
-        uids = self.test_users
-        num_users = len(uids)
-        for idx_uid in tqdm(range(num_users)):
-            uid = uids[idx_uid]
-            iids= list(range(self.train_consumption_matrix.shape[1]))
-            random.shuffle(iids)
-            self.results[uid].extend(iids[:self.interactions*self.interaction_size])
-        self.save_results()
+    def train(self,train_dataset):
+        super().train(train_dataset)
+
+    def predict(self,uid,candidate_items,num_req_items):
+        return np.random.rand(len(candidate_items)), None
+        # uids = self.test_users
+        # num_users = len(uids)
+        # for idx_uid in tqdm(range(num_users)):
+        #     uid = uids[idx_uid]
+        #     iids= list(range(self.train_consumption_matrix.shape[1]))
+        #     random.shuffle(iids)
+        #     self.results[uid].extend(iids[:self.interactions*self.interaction_size])
+        # self.save_results()
         
