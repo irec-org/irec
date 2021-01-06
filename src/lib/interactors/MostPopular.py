6144a63bf510e69851ce0bd2c4f671aea8797175
heitor57 <heitorwerneck@hotmail.com>
2021-01-06 02:40:24 -0300

    MostPopular refactor

--- src/lib/interactors/MostPopular.py
+++ src/lib/interactors/MostPopular.py
@@ -10,11 +10,6 @@ class MostPopular(Interactor):
 
     @staticmethod
     def get_items_popularity(consumption_matrix, normalize=True):
-        # uids = test_uids
-        # num_users = len(uids)
-        # mask = np.ones(consumption_matrix.shape[0], dtype=bool)
-        # mask[uids] = 0
-        # num_train_users = np.count_nonzero(mask)
         lowest_value = np.min(consumption_matrix)
         if not isinstance(consumption_matrix,scipy.sparse.spmatrix):
             items_popularity = np.count_nonzero(consumption_matrix>lowest_value,axis=0)
@@ -26,25 +21,34 @@ class MostPopular(Interactor):
                 
         return items_popularity
 
-    def interact(self):
-        super().interact()
+    def train(self,train_dataset):
+        super().train(train_dataset)
+        self.train_dataset = train_dataset
+        self.train_consumption_matrix = scipy.sparse.csr_matrix((self.train_dataset.data[2],(self.train_dataset.data[0],self.train_dataset.data[1])),(self.train_dataset.users_num,self.train_dataset.items_num))
         # num_users = len(uids)
         # mask = np.ones(self.consumption_matrix.shape[0], dtype=bool)
         # mask[uids] = 0
         # num_train_users = np.count_nonzero(mask)
 
-        items_popularity = self.get_items_popularity(self.train_consumption_matrix)
+        self.items_popularity = self.get_items_popularity(self.train_consumption_matrix)
 
-        fig, ax = plt.subplots()
-        ax.hist(items_popularity,color='k')
-        ax.set_xlabel("Popularity")
-        ax.set_ylabel("#Items")
-        fig.savefig(os.path.join(self.DIRS['img'],"popularity_"+self.get_name()+".png"))
+    def predict(self,uid,candidate_items,num_req_items):
+        items_score = self.items_popularity[candidate_items]
+        return items_score, None
 
-        top_iids = list(reversed(np.argsort(items_popularity)))[:self.get_iterations()]
-        uids = self.test_users
-        num_users = len(uids)
-        for idx_uid in tqdm(range(num_users)):
-            uid = uids[idx_uid]
-            self.results[uid].extend(top_iids)
-        self.save_results()
+    def update(self,uid,item,reward,additional_data):
+        pass
+
+        # fig, ax = plt.subplots()
+        # ax.hist(items_popularity,color='k')
+        # ax.set_xlabel("Popularity")
+        # ax.set_ylabel("#Items")
+        # fig.savefig(os.path.join(self.DIRS['img'],"popularity_"+self.get_name()+".png"))
+
+        # top_iids = list(reversed(np.argsort(items_popularity)))[:self.get_iterations()]
+        # uids = self.test_users
+        # num_users = len(uids)
+        # for idx_uid in tqdm(range(num_users)):
+        #     uid = uids[idx_uid]
+        #     self.results[uid].extend(top_iids)
+        # self.save_results()
