ad4599e865c14edd8666cac1ee0ef0f225a55c84
heitor57 <heitorwerneck@hotmail.com>
2021-01-06 03:33:52 -0300

    OurMethod2 refactor

--- src/lib/interactors/OurMethod2.py
+++ src/lib/interactors/OurMethod2.py
@@ -15,12 +15,15 @@ class OurMethod2(interactors.Interactor):
         super().__init__(*args, **kwargs)
         self.alpha = alpha
 
-    def interact(self, items_latent_factors):
-        super().interact()
-        uids = self.test_users
-        num_lat = len(items_latent_factors[0])
-        self.items_latent_factors = items_latent_factors
-        num_users = len(uids)
+    def train(self,train_dataset):
+        super().train(train_dataset)
+        self.train_dataset = train_dataset
+        self.train_consumption_matrix = scipy.sparse.csr_matrix((self.train_dataset.data[2],(self.train_dataset.data[0],self.train_dataset.data[1])),(self.train_dataset.users_num,self.train_dataset.items_num))
+        self.num_items = self.train_dataset.num_items
+
+        mf_model = mf.SVD()
+        mf_model.fit(self.train_consumption_matrix)
+        self.items_weights = mf_model.items_weights
 
         items_entropy = interactors.Entropy.get_items_entropy(self.train_consumption_matrix)
         items_popularity = interactors.MostPopular.get_items_popularity(self.train_consumption_matrix,normalize=False)
@@ -29,64 +32,30 @@ class OurMethod2(interactors.Interactor):
         assert(self.items_bias.min() >= 0 and self.items_bias.max() == 1)
 
         # regression_model = sklearn.linear_model.LinearRegression()
-        res=scipy.optimize.minimize(lambda x,items_latent_factors,items_bias: np.linalg.norm(items_bias - x @ items_latent_factors.T),
+        res=scipy.optimize.minimize(lambda x,items_weights,items_bias: np.linalg.norm(items_bias - x @ items_weights.T),
                                     np.ones(num_lat),
-                                    args=(self.items_latent_factors,self.items_bias))
+                                    args=(self.items_weights,self.items_bias))
         self.initial_b = res.x 
 
-        print(np.corrcoef(self.items_bias,self.initial_b @ items_latent_factors.T)[0,1])
-        # users_bs = {}
-        # for uid in uids:
-        #     users_bs[uid] = np.ones(num_lat)
-        #     users_bs[uid] = res.x
-        self_id = id(self)
-        np.seterr('warn')
-        with threadpool_limits(limits=1, user_api='blas'):
-            args = [(self_id,int(uid),) for uid in uids]
-            results = util.run_parallel(self.interact_user,args)
+        print(np.corrcoef(self.items_bias,self.initial_b @ items_weights.T)[0,1])
 
-        for i, user_result in enumerate(results):
-            self.results[uids[i]] = user_result
+        self.I = np.eye(len(self.items_weights[0]))
+        bs = defaultdict(lambda: np.zeros(self.num_latent_factors))
+        As = defaultdict(lambda: self.initial_b.copy())
 
-        self.save_results()
+    def predict(self,uid,candidate_items,num_req_items):
+        b = bs[uid]
+        A = As[uid]
+        user_latent_factors = np.dot(np.linalg.inv(A),b)
+        items_uncertainty = np.sqrt(np.sum(self.items_weights[user_candidate_items].dot(np.linalg.inv(A)) * self.items_weights[user_candidate_items],axis=1))
+        items_user_similarity = user_latent_factors @ self.items_weights[user_candidate_items].T
+        user_model_items_score = items_user_similarity + self.alpha*items_uncertainty
+        items_score = user_model_items_score
+        return items_score, None
 
-    @staticmethod
-    def interact_user(obj_id,uid):
-        self = ctypes.cast(obj_id, ctypes.py_object).value
-        num_lat = len(self.items_latent_factors[0])
-        I = np.eye(num_lat)
-
-        user_candidate_items = np.array(list(range(len(self.items_latent_factors))))
-        num_user_candidate_items = len(user_candidate_items)
-        b = self.initial_b.copy()
-        # regression_model = sklearn.linear_model.LinearRegression()
-        # (
-        #     self.items_latent_factors)
-        A = I.copy()
-        result = []
-        for i in range(self.interactions):
-            user_latent_factors = np.dot(np.linalg.inv(A),b)
-            items_uncertainty = np.sqrt(np.sum(self.items_latent_factors[user_candidate_items].dot(np.linalg.inv(A)) * self.items_latent_factors[user_candidate_items],axis=1))
-            items_user_similarity = user_latent_factors @ self.items_latent_factors[user_candidate_items].T
-            user_model_items_score = items_user_similarity + self.alpha*items_uncertainty
-            items_score = user_model_items_score
-
-            best_items = user_candidate_items[np.argsort(items_score)[::-1]][:self.interaction_size]
-            user_candidate_items = user_candidate_items[~np.isin(user_candidate_items,best_items)]
-            result.extend(best_items)
-
-            # num_correct_items_history.append(0)
-            for max_i in result[i*self.interaction_size:(i+1)*self.interaction_size]:
-                max_item_latent_factors = self.items_latent_factors[max_i]
-                A += max_item_latent_factors[:,None].dot(max_item_latent_factors[None,:])
-                num_user_candidate_items -= 1
-                if self.get_reward(uid,max_i) >= self.threshold:
-                    b += self.get_reward(uid,max_i)*max_item_latent_factors
-                    if self.exit_when_consumed_all and num_correct_items == self.users_num_correct_items[uid]:
-                        print(f"Exiting user {uid} with {len(result)} items in total and {num_correct_items} correct ones")
-                        return np.array(result)
-
-                    
-            # old_mean = mean.copy()
-
-        return np.array(result)
+    def update(self,uid,item,reward,additional_data):
+        max_item_latent_factors = self.items_weights[item]
+        b = bs[uid]
+        A = As[uid]
+        A += max_item_latent_factors[:,None].dot(max_item_latent_factors[None,:])
+        b += reward*max_item_latent_factors
