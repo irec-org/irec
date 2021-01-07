da49bc6c1a9ce2c3e9721e6b43ee06a2cbb725fc
heitor57 <heitorwerneck@hotmail.com>
2021-01-06 21:37:55 -0300

    Thompson Sampling refactor

--- src/lib/interactors/ThompsonSampling.py
+++ src/lib/interactors/ThompsonSampling.py
@@ -9,61 +9,27 @@ class ThompsonSampling(ExperimentalInteractor):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def interact(self):
-        super().interact()
-        uids = self.test_users
-        num_users = len(uids)
-        num_items = self.train_consumption_matrix.shape[1]
-        
-        # self.beta_distributions = [scipy.stats.beta(a=1,b=1) for item in range(num_items)]
-        # print(self.beta_distributions[0].a)
-        self.alphas = np.ones(num_items)
-        self.betas = np.ones(num_items)
+    def train(self,train_dataset):
+        super().train(train_dataset)
+        self.train_dataset = train_dataset
+        self.train_consumption_matrix = scipy.sparse.csr_matrix((self.train_dataset.data[2],(self.train_dataset.data[0],self.train_dataset.data[1])),(self.train_dataset.users_num,self.train_dataset.items_num))
+        self.num_items = self.train_dataset.num_items
 
-        users_num_interactions = defaultdict(int)
-        available_users = set(uids)
+        self.alphas = np.ones(self.num_items)
+        self.betas = np.ones(self.num_items)
 
-        # mask = np.ones(self.consumption_matrix.shape[0], dtype=bool)
-        # mask[uids] = 0
-        if not self.is_spmatrix:
-            self.alphas += np.count_nonzero(self.train_consumption_matrix>=self.threshold,
-                                            axis=0)
-            self.betas += np.count_nonzero(self.train_consumption_matrix<self.threshold,
-                                           axis=0)
-        else:
-            a = np.sum(self.train_consumption_matrix>=self.threshold,
-                                            axis=0).A.flatten()
-            self.alphas += a
-            self.betas += self.train_consumption_matrix.shape[0] - a
-            del a
-        #     print(self.alphas)
+        a = np.sum(self.train_consumption_matrix>=self.threshold,
+                                        axis=0).A.flatten()
+        self.alphas += a
+        self.betas += self.train_consumption_matrix.shape[0] - a
 
-        # print("WQEWQ")
-        for i in trange(num_users*self.interactions):
+    def predict(self,uid,candidate_items,num_req_items):
+        items_score = np.random.beta(self.alphas[candidate_items],
+                                     self.betas[candidate_items])
+        return items_score, None
 
-            uid = random.sample(available_users,k=1)[0]
-
-            # best_item = np.argmax([self.beta_distributions[item].rvs() for item in range(num_items)])
-            not_recommended = np.ones(num_items,dtype=bool)
-            not_recommended[self.results[uid]] = 0
-            items_not_recommended = np.nonzero(not_recommended)[0]
-            items_score = np.random.beta(self.alphas[items_not_recommended],
-                                         self.betas[items_not_recommended])
-            top_items = list(reversed(np.argsort(items_score)))[:self.interaction_size]
-            best_items = items_not_recommended[top_items]
-            self.results[uid].extend(best_items)
-            # reward = (self.get_reward(uid,best_item)-self.lowest_value)/(self.highest_value-self.lowest_value)
-            # reward = 1 if reward >= 0.8 else 0
-
-            user_num_interactions = users_num_interactions[uid]
-
-            for best_item in self.results[uid][user_num_interactions*self.interaction_size:(user_num_interactions+1)*self.interaction_size]:
-                reward = self.get_reward(uid,best_item)
-                reward = 1 if reward >= self.threshold else 0
-                self.alphas[best_item] += reward
-                self.betas[best_item] += 1-reward
-
-            users_num_interactions[uid] += 1
-            if users_num_interactions[uid] == self.interactions:
-                available_users = available_users - {uid}
-        self.save_results()
+    def update(self,uid,item,reward,additional_data):
+        reward = reward
+        reward = 1 if reward >= min(self.train_dataset.rate_domain) else 0
+        self.alphas[best_item] += reward
+        self.betas[best_item] += 1-reward
