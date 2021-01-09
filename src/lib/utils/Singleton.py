e3d61be32920c6b55a3ca1658e3428f788952284
heitor57 <heitorwerneck@hotmail.com>
2021-01-08 21:13:13 -0300

    removed singleton

--- src/lib/utils/Singleton.py
+++
@@ -1,7 +0,0 @@
-class Singleton:
-    _instance = None
-    @classmethod
-    def getInstance(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance=cls(*args,**kwargs)
-        return cls._instance
