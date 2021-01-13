7268d39117f852f2c186bd8477e7bcb32322b9fa
heitor57 <heitorwerneck@hotmail.com>
2021-01-13 12:28:54 -0300

    Just one Most Popular

--- src/lib/interactors/MostPopular.py
+++ src/lib/interactors/MostPopular.py
@@ -1,9 +1,12 @@
 import numpy as np
 from tqdm import tqdm
 from .ExperimentalInteractor import ExperimentalInteractor
+from .MostPopular import *
 import matplotlib.pyplot as plt
 import os
 import scipy.sparse
+from collections import defaultdict
+import random
 class MostPopular(ExperimentalInteractor):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -24,31 +27,13 @@ class MostPopular(ExperimentalInteractor):
     def train(self,train_dataset):
         super().train(train_dataset)
         self.train_dataset = train_dataset
+        self.num_items = self.train_dataset.num_items
         self.train_consumption_matrix = scipy.sparse.csr_matrix((self.train_dataset.data[:,2],(self.train_dataset.data[:,0],self.train_dataset.data[:,1])),(self.train_dataset.users_num,self.train_dataset.items_num))
-        # num_users = len(uids)
-        # mask = np.ones(self.consumption_matrix.shape[0], dtype=bool)
-        # mask[uids] = 0
-        # num_train_users = np.count_nonzero(mask)
-
-        self.items_popularity = self.get_items_popularity(self.train_consumption_matrix)
+        self.items_popularity = self.get_items_popularity(self.train_consumption_matrix, normalize=False)
 
     def predict(self,uid,candidate_items,num_req_items):
         items_score = self.items_popularity[candidate_items]
         return items_score, None
 
     def update(self,uid,item,reward,additional_data):
-        pass
-
-        # fig, ax = plt.subplots()
-        # ax.hist(items_popularity,color='k')
-        # ax.set_xlabel("Popularity")
-        # ax.set_ylabel("#Items")
-        # fig.savefig(os.path.join(self.DIRS['img'],"popularity_"+self.get_id()+".png"))
-
-        # top_iids = list(reversed(np.argsort(items_popularity)))[:self.get_iterations()]
-        # uids = self.test_users
-        # num_users = len(uids)
-        # for idx_uid in tqdm(range(num_users)):
-        #     uid = uids[idx_uid]
-        #     self.results[uid].extend(top_iids)
-        # self.save_results()
+        self.items_popularity[item] += 1
--- src/lib/utils/InteractorCache.py
+++ src/lib/utils/InteractorCache.py
@@ -1,4 +1,3 @@
-
 class InteractorCache:
     def get_id(self,dm,evaluation_policy,itr):
         return dm.get_id()+'/'+evaluation_policy.get_id()+'/'+itr.get_id()
