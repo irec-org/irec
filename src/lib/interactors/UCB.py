4d872e06be83fcc803aa2c4c2bb533e969c06339
heitor57 <heitorwerneck@hotmail.com>
2021-01-23 17:58:27 -0300

    UCB update and fixes, exception treatment and etc

--- src/lib/interactors/UCB.py
+++ src/lib/interactors/UCB.py
@@ -5,8 +5,10 @@ import os
 import random
 import scipy.stats
 from collections import defaultdict
+
+
 class UCB(ExperimentalInteractor):
-    def __init__(self,c=1.0,*args, **kwargs):
+    def __init__(self,c,*args, **kwargs):
         super().__init__(*args, **kwargs)
         self.c = c
         self.parameters.extend(['c'])
@@ -22,20 +24,28 @@ class UCB(ExperimentalInteractor):
 
         self.t = 1
 
-        users_num_interactions = defaultdict(int)
-        available_users = set(uids)
+        # users_num_interactions = defaultdict(int)
+        # available_users = set(uids)
 
-        mask = np.ones(self.train_consumption_matrix.shape[0], dtype=bool)
-        mask[uids] = 0
-        self.items_mean_values = np.mean(self.train_consumption_matrix[mask],axis=0).A.flatten()
-        self.items_count += self.train_consumption_matrix[mask].shape[0]
-        self.t += np.prod(self.train_consumption_matrix[mask].shape)
+        for i in range(self.train_dataset.data.shape[0]):
+            uid = self.train_dataset.data[i,0]
+            item = self.train_dataset.data[i,1]
+            reward = self.train_dataset.data[i,2]
+            self.update(uid,item,reward,None)
+            self.increment_time()
+        # mask = np.ones(self.train_consumption_matrix.shape[0], dtype=bool)
+        # mask[uids] = 0
+        # self.items_mean_values = np.mean(self.train_consumption_matrix[mask],axis=0).A.flatten()
+        # self.items_count += self.train_consumption_matrix[mask].shape[0]
+        # self.t += np.prod(self.train_consumption_matrix[mask].shape)
 
     def predict(self,uid,candidate_items,num_req_items):
-        items_uncertainty = self.c*np.sqrt(2*np.log(self.t)/self.items_count[candidate_items])
-        items_score = self.items_mean_values[candidate_items]+items_uncertainty
+        with np.errstate(divide='ignore'):
+            items_uncertainty = self.c*np.sqrt(2*np.log(self.t)/self.items_count[candidate_items])
+            items_score = self.items_mean_values[candidate_items]+items_uncertainty
         return items_score, None
 
     def update(self,uid,item,reward,additional_data):
-        self.items_mean_values[best_item] = (self.items_mean_values[best_item]*self.items_count[best_item]+self.get_reward(uid,best_item))/(self.items_count[best_item] + 1)
-        self.items_count[best_item] += 1
+        item = int(item)
+        self.items_mean_values[item] = (self.items_mean_values[item]*self.items_count[item]+reward)/(self.items_count[item] + 1)
+        self.items_count[item] += 1
