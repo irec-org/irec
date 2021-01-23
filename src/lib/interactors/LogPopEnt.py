4f9ef45c6ef88b509ee61dec8723dd3470bce5e8
heitor57 <heitorwerneck@hotmail.com>
2021-01-22 23:49:30 -0300

    LogPopEnt comments clear

--- src/lib/interactors/LogPopEnt.py
+++ src/lib/interactors/LogPopEnt.py
@@ -29,32 +29,3 @@ class LogPopEnt(ExperimentalInteractor):
 
     def update(self,uid,item,reward,additional_data):
         pass
-        # correlation = scipy.stats.pearsonr(items_entropy,items_popularity)[0]
-
-        # top_iids = list(reversed(np.argsort(items_logpopent)))[:self.get_iterations()]
-
-        # fig, ax = plt.subplots()
-        # ax.hist(items_entropy,color='k')
-        # ax.set_xlabel("$\log(Pop)\cdot Ent$")
-        # ax.set_ylabel("#Items")
-        # fig.savefig(os.path.join(self.DIRS['img'],"logpopent_"+self.get_id()+".png"))
-
-
-        # fig, ax = plt.subplots()
-        # ax.scatter(items_entropy,items_popularity,marker="D",color='darkblue')
-        # ax.set_ylabel("Popularity")
-        # ax.set_xlabel("Entropy")
-        # ax.text(0.3, 0.9 , f'Correlation coefficient: {correlation:.2f}', color='k',
-        #         ha='center', va='center',
-        #         bbox=dict(facecolor='none', edgecolor='k', pad=10.0),
-        #         transform = ax.transAxes)
-        # for start, end, color in [(0,10,'green'),(10,20,'red'),(20,30,'darkred'),(30,40,'yellow'),(40,50,'orange')]:
-        #     ax.scatter(items_entropy[top_iids[start:end]],items_popularity[top_iids[start:end]],marker='D',color=color)
-        # fig.savefig(os.path.join(self.DIRS['img'],"corr_popent_"+self.get_id()+".png"))
-
-
-        # num_total_users = len(uids)
-        # for idx_uid in tqdm(range(num_total_users)):
-        #     uid = uids[idx_uid]
-        #     self.results[uid].extend(top_iids)
-        # self.save_results()
