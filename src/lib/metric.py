a17a02ff5767c3b142f9440de1293c3a45f27a0c
heitor57 <heitorwerneck@hotmail.com>
2021-01-23 21:27:14 -0300

    Parallel interaction metrics evaluator and logical error fix
    The logical error is just the metric object creation at the wrong position

--- src/lib/metric.py
+++ src/lib/metric.py
@@ -65,29 +65,39 @@ class InteractionMetricsEvaluator(MetricsEvaluator):
         self.ground_truth_dataset = ground_truth_dataset
         self.ground_truth_consumption_matrix = scipy.sparse.csr_matrix((self.ground_truth_dataset.data[:,2],(self.ground_truth_dataset.data[:,0],self.ground_truth_dataset.data[:,1])),(self.ground_truth_dataset.num_total_users,self.ground_truth_dataset.num_total_items))
 
+    @staticmethod
+    def _metric_evaluation(obj_id,num_interactions, interaction_size, metric_class):
+        self = ctypes.cast(obj_id,ctypes.py_object).value
+        start_time = time.time()
+        metric_values = []
+        for i in range(num_interactions):
+            metric = metric_class(self.ground_truth_dataset,
+                                  ThresholdRelevanceEvaluator(0)
+                                  )
+            for uid in self.uids:
+                interaction_results = self.users_items_recommended[uid][i:i+interaction_size]
+                for item in interaction_results:
+                    metric.update_recommendation(uid,item,self.ground_truth_consumption_matrix[uid,item])
+
+            metric_values.append(np.mean([metric.compute(uid) for uid in self.uids]))
+
+        print(f"{self.__class__.__name__} spent {time.time()-start_time:.2f} seconds executing {metric_class.__name__} metric")
+        return metric_values
+
     def evaluate(self,num_interactions,interaction_size,results):
-        users_items_recommended = defaultdict(list)
+        self.users_items_recommended = defaultdict(list)
         for uid, item in results:
-            users_items_recommended[uid].append(item)
-        uids = list(users_items_recommended.keys())
+            self.users_items_recommended[uid].append(item)
+        self.uids = list(self.users_items_recommended.keys())
         
         metrics_values = defaultdict(list)
-        
-        # for uid, item in results:
-        for metric_class in self.metrics_classes:
-            metric = metric_class(self.ground_truth_dataset,
-                                    ThresholdRelevanceEvaluator(0)
-                                  )
-            start_time = time.time()
-            for i in range(num_interactions):
-                for uid in uids:
-                    interaction_results = users_items_recommended[uid][i:i+interaction_size]
-                    for item in interaction_results:
-                        metric.update_recommendation(uid,item,self.ground_truth_consumption_matrix[uid,item])
-
-                metrics_values[metric.__class__.__name__].append(np.mean([metric.compute(uid) for uid in uids]))
-
-            print(f"{self.__class__.__name__} spent {time.time()-start_time:.2f} seconds executing {metric_class.__name__} metric")
+        results = run_parallel(self._metric_evaluation,
+                               [(id(self), num_interactions,
+                                 interaction_size, metric_class)
+                                for metric_class in self.metrics_classes],
+                               use_tqdm=False)
+        for result, metric_class in zip(results,self.metrics_classes):
+            metrics_values[metric_class.__name__].extend(result)
 
         return metrics_values
 
