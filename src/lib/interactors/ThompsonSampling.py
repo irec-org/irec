abf6a8f3d5ff2d5fe37977d15a3dc891c21c766c
heitor57 <heitorwerneck@hotmail.com>
2021-01-23 23:32:14 -0300

    Thompson Sampling with alpha and beta initial parameters

--- src/lib/interactors/ThompsonSampling.py
+++ src/lib/interactors/ThompsonSampling.py
@@ -6,8 +6,11 @@ import random
 import scipy.stats
 from collections import defaultdict
 class ThompsonSampling(ExperimentalInteractor):
-    def __init__(self,*args, **kwargs):
+    def __init__(self,alpha_0, beta_0, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.alpha_0 = alpha_0
+        self.beta_0 = beta_0
+        self.parameters.extend(['alpha_0','beta_0'])
 
     def train(self,train_dataset):
         super().train(train_dataset)
@@ -15,8 +18,8 @@ class ThompsonSampling(ExperimentalInteractor):
         self.train_consumption_matrix = scipy.sparse.csr_matrix((self.train_dataset.data[:,2],(self.train_dataset.data[:,0],self.train_dataset.data[:,1])),(self.train_dataset.num_total_users,self.train_dataset.num_total_items))
         self.num_total_items = self.train_dataset.num_total_items
 
-        self.alphas = np.ones(self.num_total_items)
-        self.betas = np.ones(self.num_total_items)
+        self.alphas = np.ones(self.num_total_items)*self.alpha_0
+        self.betas = np.ones(self.num_total_items)*self.beta_0
 
         a = np.sum(self.train_consumption_matrix>=self.train_dataset.mean_rating,
                                         axis=0).A.flatten()
@@ -30,6 +33,6 @@ class ThompsonSampling(ExperimentalInteractor):
 
     def update(self,uid,item,reward,additional_data):
         reward = reward
-        reward = 1 if reward >= self.train_dataset.mean_rating else 0
+        reward = 1 if reward > self.train_dataset.mean_rating else 0
         self.alphas[item] += reward
         self.betas[item] += 1-reward
