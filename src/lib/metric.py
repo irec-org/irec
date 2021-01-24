d5e64cfefa7fdd026382c47659b4b937c8a18d65
heitor57 <heitorwerneck@hotmail.com>
2021-01-24 02:49:39 -0300

    Positive ratings greater than mean in metrics evaluators

--- src/lib/metric.py
+++ src/lib/metric.py
@@ -12,7 +12,6 @@ class MetricsEvaluator(Parameterizable):
     def __init__(self,metrics_classes=[],*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.metrics_classes = metrics_classes
-        self.relevance_evaluator = ThresholdRelevanceEvaluator(0)
 
 class CumulativeMetricsEvaluator(MetricsEvaluator):
 
@@ -21,6 +20,7 @@ class CumulativeMetricsEvaluator(MetricsEvaluator):
         self.ground_truth_dataset = ground_truth_dataset
         self.ground_truth_consumption_matrix = scipy.sparse.csr_matrix((self.ground_truth_dataset.data[:,2],(self.ground_truth_dataset.data[:,0],self.ground_truth_dataset.data[:,1])),(self.ground_truth_dataset.num_total_users,self.ground_truth_dataset.num_total_items))
         self.buffer_size = buffer_size
+        self.relevance_evaluator = ThresholdRelevanceEvaluator(self.ground_truth_dataset.mean_rating)
         self.parameters.extend(['buffer_size'])
 
     @staticmethod
@@ -78,6 +78,7 @@ class InteractionMetricsEvaluator(MetricsEvaluator):
         super().__init__(*args,**kwargs)
         self.ground_truth_dataset = ground_truth_dataset
         self.ground_truth_consumption_matrix = scipy.sparse.csr_matrix((self.ground_truth_dataset.data[:,2],(self.ground_truth_dataset.data[:,0],self.ground_truth_dataset.data[:,1])),(self.ground_truth_dataset.num_total_users,self.ground_truth_dataset.num_total_items))
+        self.relevance_evaluator = ThresholdRelevanceEvaluator(self.ground_truth_dataset.mean_rating)
 
     @staticmethod
     def _metric_evaluation(obj_id,num_interactions, interaction_size, metric_class):
