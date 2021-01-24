d55014088f7e28a0f5be58e8b401753f401e7788
heitor57 <heitorwerneck@hotmail.com>
2021-01-23 23:14:07 -0300

    Cross-Platform path: interactor cache file path

--- src/lib/utils/InteractorCache.py
+++ src/lib/utils/InteractorCache.py
@@ -1,3 +1,4 @@
+import os
 class InteractorCache:
     def get_id(self,dm,evaluation_policy,itr):
-        return dm.get_id()+'/'+evaluation_policy.get_id()+'/'+itr.get_id()
+        return os.path.join(dm.get_id(),evaluation_policy.get_id(),itr.get_id())
