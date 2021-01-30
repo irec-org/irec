9e7fd0ddb7b005f28d6e193c63883393fab706ea
heitor57 <heitorwerneck@hotmail.com>
2021-01-30 02:07:37 -0300

    Sampled datasets

--- src/app/eval.py
+++ src/app/eval.py
@@ -20,64 +20,67 @@ import metric
 from utils.util import run_parallel
 import ctypes
 
-metrics_classes = [metric.Precision,metric.Recall,metric.Hits]
+metrics_classes = [metric.Precision, metric.Recall, metric.Hits]
 
 dm = DatasetManager()
 dm.request_dataset_preprocessor()
 dm.initialize_engines()
 dm.load()
 
-interactors_preprocessor_paramaters = yaml.load(open("settings"+sep+"interactors_preprocessor_parameters.yaml"),Loader=yaml.SafeLoader)
-interactors_general_settings = yaml.load(open("settings"+sep+"interactors_general_settings.yaml"),Loader=yaml.SafeLoader)
+interactors_preprocessor_paramaters = yaml.load(
+    open("settings" + sep + "interactors_preprocessor_parameters.yaml"),
+    Loader=yaml.SafeLoader)
+interactors_general_settings = yaml.load(
+    open("settings" + sep + "interactors_general_settings.yaml"),
+    Loader=yaml.SafeLoader)
 
-evaluation_policies_parameters = yaml.load(open("settings"+sep+"evaluation_policies_parameters.yaml"),Loader=yaml.SafeLoader)
+evaluation_policies_parameters = yaml.load(
+    open("settings" + sep + "evaluation_policies_parameters.yaml"),
+    Loader=yaml.SafeLoader)
 
-ir = InteractorRunner(dm,interactors_general_settings,interactors_preprocessor_paramaters,evaluation_policies_parameters)
+ir = InteractorRunner(dm, interactors_general_settings,
+                      interactors_preprocessor_paramaters,
+                      evaluation_policies_parameters)
 interactors_classes = ir.select_interactors()
 
-data = np.vstack((dm.dataset_preprocessed[0].data,
-                  dm.dataset_preprocessed[1].data))
+data = np.vstack(
+    (dm.dataset_preprocessed[0].data, dm.dataset_preprocessed[1].data))
 
 dataset = Dataset(data)
 dataset.update_from_data()
 dataset.update_num_total_users_items()
 
-metrics_evaluators = [InteractionMetricsEvaluator(dataset,metrics_classes), CumulativeMetricsEvaluator(50,dataset,metrics_classes)]
+metrics_evaluators = [
+    InteractionMetricsEvaluator(dataset, metrics_classes),
+    CumulativeMetricsEvaluator(50, dataset, metrics_classes)
+]
 
 
 def evaluate_itr(metric_evaluator_id, dm_id, itr_class):
-    metric_evaluator = ctypes.cast(metric_evaluator_id,ctypes.py_object).value
-    dm = ctypes.cast(dm_id,ctypes.py_object).value
+    metric_evaluator = ctypes.cast(metric_evaluator_id, ctypes.py_object).value
+    dm = ctypes.cast(dm_id, ctypes.py_object).value
     print(f"Evaluating {itr_class.__name__} results")
     itr = ir.create_interactor(itr_class)
     evaluation_policy = ir.get_interactor_evaluation_policy(itr)
     pdm = PersistentDataManager(directory='results')
-    users_items_recommended = pdm.load(InteractorCache().get_id(dm,evaluation_policy,itr))
+    users_items_recommended = pdm.load(InteractorCache().get_id(
+        dm, evaluation_policy, itr))
 
     metrics_pdm = PersistentDataManager(directory='metrics')
-    if isinstance(metric_evaluator,InteractionMetricsEvaluator):
-        metrics_values = metric_evaluator.evaluate(evaluation_policy.num_interactions,evaluation_policy.interaction_size,users_items_recommended)
-    elif isinstance(metric_evaluator,CumulativeMetricsEvaluator):
+    if isinstance(metric_evaluator, InteractionMetricsEvaluator):
+        metrics_values = metric_evaluator.evaluate(
+            evaluation_policy.num_interactions,
+            evaluation_policy.interaction_size, users_items_recommended)
+    elif isinstance(metric_evaluator, CumulativeMetricsEvaluator):
         metrics_values = metric_evaluator.evaluate(users_items_recommended)
 
     for metric_name, metric_values in metrics_values.items():
-        metrics_pdm.save(os.path.join(InteractorCache().get_id(dm,evaluation_policy,itr),metric_evaluator.get_id(),metric_name),metric_values)
+        metrics_pdm.save(
+            os.path.join(InteractorCache().get_id(dm, evaluation_policy, itr),
+                         metric_evaluator.get_id(), metric_name), metric_values)
+
 
 for metric_evaluator in metrics_evaluators:
-    args = [(id(metric_evaluator),id(dm),itr_class) for itr_class in interactors_classes]
-    run_parallel(evaluate_itr,args,use_tqdm=False)
-    # for itr_class in interactors_classes:
-    #     print(f"Evaluating {itr_class.__name__} results")
-    #     itr = ir.create_interactor(itr_class)
-    #     evaluation_policy = ir.get_interactor_evaluation_policy(itr)
-    #     pdm = PersistentDataManager(directory='results')
-    #     users_items_recommended = pdm.load(InteractorCache().get_id(dm,evaluation_policy,itr))
-
-    #     metrics_pdm = PersistentDataManager(directory='metrics')
-    #     if isinstance(metric_evaluator,InteractionMetricsEvaluator):
-    #         metrics_values = metric_evaluator.evaluate(evaluation_policy.num_interactions,evaluation_policy.interaction_size,users_items_recommended)
-    #     elif isinstance(metric_evaluator,CumulativeMetricsEvaluator):
-    #         metrics_values = metric_evaluator.evaluate(users_items_recommended)
-
-    #     for metric_name, metric_values in metrics_values.items():
-    #         metrics_pdm.save(os.path.join(InteractorCache().get_id(dm,evaluation_policy,itr),metric_evaluator.get_id(),metric_name),metric_values)
+    args = [(id(metric_evaluator), id(dm), itr_class)
+            for itr_class in interactors_classes]
+    run_parallel(evaluate_itr, args, use_tqdm=False)
--- src/lib/utils/dataset.py
+++ src/lib/utils/dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import interactors
 from collections import defaultdict
 import random
 import math
@@ -12,36 +13,52 @@ import os
 from copy import copy
 from .Parameterizable import Parameterizable
 
+
 class DatasetPreprocessor(Parameterizable):
-    def __init__(self, name, dataset_descriptor,preprocessor,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+
+    def __init__(self, name, dataset_descriptor, preprocessor, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.name = name
         self.dataset_descriptor = dataset_descriptor
         self.preprocessor = preprocessor
-        self.parameters.extend(['dataset_descriptor','preprocessor'])
+        self.parameters.extend(['dataset_descriptor', 'preprocessor'])
 
-    def get_id(self,*args,**kwargs):
-        return super().get_id(len(self.parameters),*args,**kwargs)
+    def get_id(self, *args, **kwargs):
+        return super().get_id(len(self.parameters), *args, **kwargs)
+
 
 class Pipeline(Parameterizable):
-    def __init__(self,steps=[],*args,**kwargs):
-        super().__init__(*args,**kwargs)
+
+    def __init__(self, steps=[], *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.steps = steps
         self.parameters.extend(['steps'])
-    def process(self,data):
+
+    def process(self, data):
         buf = data
         for element in self.steps:
             buf = element.process(buf)
         return buf
-        
+
+
 class DatasetDescriptor(Parameterizable):
-    def __init__(self,dataset_dir,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+
+    def __init__(self, dataset_dir, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.dataset_dir = dataset_dir
         self.parameters.extend(['dataset_dir'])
 
+
 class Dataset:
-    def __init__(self,data,num_total_users=None,num_total_items=None,num_users=None,num_items=None,rate_domain=None,uids=None):
+
+    def __init__(self,
+                 data,
+                 num_total_users=None,
+                 num_total_items=None,
+                 num_users=None,
+                 num_items=None,
+                 rate_domain=None,
+                 uids=None):
         self.data = data
         self.num_users = num_users
         self.num_items = num_items
@@ -51,29 +68,35 @@ class Dataset:
         self.num_total_items = num_total_items
 
     def update_from_data(self):
-        self.num_users = len(np.unique(self.data[:,0]))
-        self.num_items = len(np.unique(self.data[:,1]))
-        self.rate_domain  = set(np.unique(self.data[:,2]))
-        self.uids = np.unique(self.data[:,0])
-        self.mean_rating = np.mean(self.data[:,2])
-        self.min_rating = np.min(self.data[:,2])
-        self.max_rating = np.max(self.data[:,2])
+        self.num_users = len(np.unique(self.data[:, 0]))
+        self.num_items = len(np.unique(self.data[:, 1]))
+        self.rate_domain = set(np.unique(self.data[:, 2]))
+        self.uids = np.unique(self.data[:, 0])
+        self.mean_rating = np.mean(self.data[:, 2])
+        self.min_rating = np.min(self.data[:, 2])
+        self.max_rating = np.max(self.data[:, 2])
+
     def update_num_total_users_items(self):
         self.num_total_users = self.num_users
         self.num_total_items = self.num_items
-        
+
         # self.consumption_matrix = scipy.sparse.csr_matrix((self.data[:,2],(self..data[:,0],self.train_dataset.data[:,1])),(self.train_dataset.users_num,self.train_dataset.items_num))
+
 
 class DataProcessor(Parameterizable):
     pass
 
-class TRTE(DataProcessor):
-    def process(self,dataset_descriptor):
-        dataset_dir = dataset_descriptor.dataset_dir
-        train_data = np.loadtxt(os.path.join(dataset_dir,'train.data'),delimiter='::')
-        test_data = np.loadtxt(os.path.join(dataset_dir,'test.data'),delimiter='::')
 
-        dataset = Dataset(np.vstack([train_data,test_data]))
+class TRTE(DataProcessor):
+
+    def process(self, dataset_descriptor):
+        dataset_dir = dataset_descriptor.dataset_dir
+        train_data = np.loadtxt(os.path.join(dataset_dir, 'train.data'),
+                                delimiter='::')
+        test_data = np.loadtxt(os.path.join(dataset_dir, 'test.data'),
+                               delimiter='::')
+
+        dataset = Dataset(np.vstack([train_data, test_data]))
         dataset.update_from_data()
         dataset.update_num_total_users_items()
         train_dataset = copy(dataset)
@@ -84,30 +107,36 @@ class TRTE(DataProcessor):
         test_dataset.update_from_data()
         return train_dataset, test_dataset
 
+
 class MovieLens100k(DataProcessor):
-    def process(self,dataset_descriptor):
+
+    def process(self, dataset_descriptor):
         dataset_dir = dataset_descriptor.dataset_dir
-        data = np.loadtxt(os.path.join(dataset_dir,'u.data'),delimiter='\t')
-        data[:,0] = data[:,0] - 1
-        data[:,1] = data[:,1] - 1
+        data = np.loadtxt(os.path.join(dataset_dir, 'u.data'), delimiter='\t')
+        data[:, 0] = data[:, 0] - 1
+        data[:, 1] = data[:, 1] - 1
         dataset = Dataset(data)
         dataset.update_from_data()
         dataset.update_num_total_users_items()
         return dataset
 
+
 class MovieLens1M(DataProcessor):
-    def process(self,dataset_descriptor):
+
+    def process(self, dataset_descriptor):
         dataset_dir = dataset_descriptor.dataset_dir
-        data = np.loadtxt(os.path.join(dataset_dir,'ratings.dat'),delimiter='::')
+        data = np.loadtxt(os.path.join(dataset_dir, 'ratings.dat'),
+                          delimiter='::')
         iids = dict()
-        for i, iid in enumerate(np.unique(data[:,1])):
+        for i, iid in enumerate(np.unique(data[:, 1])):
             iids[iid] = i
-        data[:,1] = np.vectorize(lambda x: iids[x])(data[:,1])
-        data[:,0] = data[:,0] - 1
+        data[:, 1] = np.vectorize(lambda x: iids[x])(data[:, 1])
+        data[:, 0] = data[:, 0] - 1
         dataset = Dataset(data)
         dataset.update_from_data()
         dataset.update_num_total_users_items()
         return dataset
+
 
 def _netflix_read_ratings(self, fileName):
     file = open(fileName, "r")
@@ -123,75 +152,151 @@ def _netflix_read_ratings(self, fileName):
     cont = 0
     for row in file:
         values = row.split('::')
-        uid, iid,rating, ts = int(float(values[0])),int(float(values[1])),values[2], int(float(values[3].replace('\n', '')))
+        uid, iid, rating, ts = int(float(values[0])), int(float(
+            values[1])), values[2], int(float(values[3].replace('\n', '')))
         usersId[cont] = uid
         itemsId[cont] = iid
         ratings[cont] = rating
         timestamp[cont] = ts
         cont += 1
 
-    print(numratings,usersId[-1],itemsId[-1],ratings[-1])
+    print(numratings, usersId[-1], itemsId[-1], ratings[-1])
     file.close()
     return usersId, itemsId, ratings, timestamp, numratings
 
+
 class Netflix:
-    def process(self,dataset_descriptor):
+
+    def process(self, dataset_descriptor):
         # base_dir = self.BASES_DIRS[self.base]
-        u_train, i_train, r_train, t_train, numr_train = _netflix_read_ratings(dataset_descriptor.dataset_dir+'train.data')
-        u_test, i_test, r_test, t_test, numr_test = _netflix_read_ratings(dataset_descriptor.dataset_dir+'test.data')
-        test_data = np.array((u_test,i_test,r_test,t_test))
-        train_data = np.array((u_train,i_train,r_train,t_train))
+        u_train, i_train, r_train, t_train, numr_train = _netflix_read_ratings(
+            dataset_descriptor.dataset_dir + 'train.data')
+        u_test, i_test, r_test, t_test, numr_test = _netflix_read_ratings(
+            dataset_descriptor.dataset_dir + 'test.data')
+        test_data = np.array((u_test, i_test, r_test, t_test))
+        train_data = np.array((u_train, i_train, r_train, t_train))
         return train_data, test_data
 
 
 class TrainTestConsumption(DataProcessor):
-    def __init__(self,train_size, test_consumes,crono,random_seed,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.train_size=train_size
-        self.test_consumes=test_consumes
+
+    def __init__(self, train_size, test_consumes, crono, random_seed, *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.train_size = train_size
+        self.test_consumes = test_consumes
         self.crono = crono
         self.random_seed = random_seed
-        self.parameters.extend(['train_size','test_consumes','crono','random_seed'])
-        
-    def process(self,dataset):
+        self.parameters.extend(
+            ['train_size', 'test_consumes', 'crono', 'random_seed'])
+
+    def process(self, dataset):
         np.random.seed(random_seed)
         data = dataset.data
-        num_users = len(np.unique(data[:,0]))
-        num_train_users = round(num_users*(self.train_size))
-        num_test_users = int(num_users-num_train_users)
+        num_users = len(np.unique(data[:, 0]))
+        num_train_users = round(num_users * (self.train_size))
+        num_test_users = int(num_users - num_train_users)
         data_df = pd.DataFrame(data)
-        users_items_consumed=data_df.groupby(0).count().iloc[:,0]
-        test_candidate_users=list(users_items_consumed[users_items_consumed>=self.test_consumes].to_dict().keys())
+        users_items_consumed = data_df.groupby(0).count().iloc[:, 0]
+        test_candidate_users = list(users_items_consumed[
+            users_items_consumed >= self.test_consumes].to_dict().keys())
         if self.crono:
             users_start_time = data_df.groupby(0).min()[3].to_numpy()
-            test_uids = np.array(list(test_candidate_users[list(reversed(np.argsort(users_start_time[test_candidate_users])))])[:num_test_users])
+            test_uids = np.array(
+                list(test_candidate_users[list(
+                    reversed(np.argsort(
+                        users_start_time[test_candidate_users])))])
+                [:num_test_users])
         else:
-            test_uids = np.array(random.sample(test_candidate_users,k=num_test_users))
+            test_uids = np.array(
+                random.sample(test_candidate_users, k=num_test_users))
             # print(test_uids)
-        train_uids = np.array(list(set(range(num_users))-set(test_uids)))
+        train_uids = np.array(list(set(range(num_users)) - set(test_uids)))
 
-        data_isin_test_uids = np.isin(data[:,0],test_uids)
+        data_isin_test_uids = np.isin(data[:, 0], test_uids)
 
         train_dataset = copy(dataset)
-        train_dataset.data = data[~data_isin_test_uids,:]
+        train_dataset.data = data[~data_isin_test_uids, :]
         dataset.update_from_data()
         test_dataset = copy(dataset)
-        test_dataset.data = data[data_isin_test_uids,:]
+        test_dataset.data = data[data_isin_test_uids, :]
         dataset.update_from_data()
-        print("Test shape:",test_dataset.data.shape)
-        print("Train shape:",train_dataset.data.shape)
+        print("Test shape:", test_dataset.data.shape)
+        print("Train shape:", train_dataset.data.shape)
         return train_dataset, test_dataset
 
+
 class TRTETrainValidation(DataProcessor):
-    def __init__(self,train_size=0.8, test_consumes=1,crono=False,random_seed=0,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.train_size=0.8
-        self.test_consumes=test_consumes
+
+    def __init__(self,
+                 train_size=0.8,
+                 test_consumes=1,
+                 crono=False,
+                 random_seed=0,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.train_size = 0.8
+        self.test_consumes = test_consumes
         self.crono = crono
         self.random_seed = self.random_seed
-        self.parameters.extend(['train_size','test_consumes','crono','random_seed'])
+        self.parameters.extend(
+            ['train_size', 'test_consumes', 'crono', 'random_seed'])
 
     def process(self, train_dataset, test_dataset):
-        ttc = TrainTestConsumption(self.train_size, self.test_consumes, self.crono, self.random_seed)
+        ttc = TrainTestConsumption(self.train_size, self.test_consumes,
+                                   self.crono, self.random_seed)
         train_dataset, test_dataset = ttc.process(train_dataset)
+        return train_dataset, test_dataset
+
+
+class TRTESample(DataProcessor):
+
+    def __init__(self, items_rate, sample_method, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.items_rate = items_rate
+        self.sample_method = sample_method
+        self.parameters.extend(['items_rate', 'sample_method'])
+
+    def process(self, train_dataset_and_test_dataset):
+        train_dataset = train_dataset_and_test_dataset[0]
+        test_dataset = train_dataset_and_test_dataset[1]
+        dataset = Dataset(np.vstack([train_dataset.data, test_dataset.data]))
+        dataset.update_from_data()
+        dataset.update_num_total_users_items()
+        consumption_matrix = scipy.sparse.csr_matrix(
+            (dataset.data[:, 2], (dataset.data[:, 0], dataset.data[:, 1])),
+            (dataset.num_total_users, dataset.num_total_items))
+        num_items_to_sample = int(self.items_rate * dataset.num_total_items)
+        if self.sample_method == 'entropy':
+            items_values = interactors.Entropy.get_items_entropy(
+                consumption_matrix)
+        elif self.sample_method == 'popularity':
+            items_values = interactors.MostPopular.get_items_popularity(
+                consumption_matrix)
+
+        best_items = np.argpartition(
+            items_values, -num_items_to_sample)[-num_items_to_sample:]
+        dataset.data = dataset.data[np.isin(dataset.data[:, 1], best_items), :]
+
+        new_iids = dict()
+        for i, iid in enumerate(np.unique(dataset.data[:, 1])):
+            new_iids[iid] = i
+        for i in range(len(dataset.data)):
+            dataset.data[i, 1] = new_iids[dataset.data[i, 1]]
+
+        dataset.update_from_data()
+        dataset.update_num_total_users_items()
+
+        train_uids = train_dataset.uids
+        test_uids = test_dataset.uids
+
+        train_dataset = copy(dataset)
+        train_dataset.data = dataset.data[np.isin(dataset.data[:, 0],
+                                                  train_uids)]
+        train_dataset.update_from_data()
+
+        test_dataset = copy(dataset)
+        test_dataset.data = dataset.data[np.isin(dataset.data[:, 0], test_uids)]
+        test_dataset.update_from_data()
         return train_dataset, test_dataset
