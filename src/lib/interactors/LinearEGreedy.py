a37f5a86713c7125e42abdfc280c5b22147b79b8
heitor57 <heitorwerneck@hotmail.com>
2021-02-02 19:33:59 -0300

    LinearEGreedy inheritance handling changed

--- src/lib/interactors/LinearEGreedy.py
+++ src/lib/interactors/LinearEGreedy.py
@@ -9,22 +9,39 @@ import scipy
 import joblib
 from utils.PersistentDataManager import PersistentDataManager
 
-class LinearEGreedy(LinEGreedy,ICF):
+
+class LinearEGreedy(LinEGreedy, ICF):
+
     def __init__(self, *args, **kwargs):
         ICF.__init__(self, *args, **kwargs)
         LinEGreedy.__init__(self, *args, **kwargs)
-    
-    def init_A(self,num_lat):
-        return self.get_user_lambda()*np.eye(num_lat)
+
+    def init_A(self, num_lat):
+        return self.get_user_lambda() * np.eye(num_lat)
 
     def _init_items_weights(self):
-        mf_model = mf.ICFPMFS(self.iterations,self.var,self.user_var,self.item_var,self.stop_criteria,num_lat=self.num_lat)
-        mf_model_id = joblib.hash((mf_model.get_id(),self.train_consumption_matrix))
+        mf_model = mf.ICFPMFS(self.iterations,
+                              self.var,
+                              self.user_var,
+                              self.item_var,
+                              self.stop_criteria,
+                              num_lat=self.num_lat)
+        mf_model_id = joblib.hash(
+            (mf_model.get_id(), self.train_consumption_matrix))
         pdm = PersistentDataManager('state_save')
         if pdm.file_exists(mf_model_id):
             mf_model = pdm.load(mf_model_id)
         else:
             mf_model.fit(self.train_consumption_matrix)
-            pdm.save(mf_model_id,mf_model)
-        
+            pdm.save(mf_model_id, mf_model)
+
         self.items_weights = mf_model.items_means
+
+    def train(self, train_dataset):
+        return super(LinEGreedy,self).train(train_dataset)
+
+    def predict(self, uid, candidate_items, num_req_items):
+        return super(LinEGreedy,self).predict(uid, candidate_items, num_req_items)
+
+    def update(self, uid, item, reward, additional_data):
+        return super(LinEGreedy,self).update(uid, item, reward, additional_data)
