665f0fc4e23096002529f17772633092a2dacbc8
heitor57 <heitorwerneck@hotmail.com>
2021-03-05 16:53:03 -0300

    Class name fix

--- src/lib/interactors/OurMethod5.py
+++ src/lib/interactors/OurMethod5.py
@@ -15,7 +15,7 @@ from collections import defaultdict
 from .MFInteractor import MFInteractor
 import interactors
 
-class OurMethod2(MFInteractor):
+class OurMethod5(MFInteractor):
     def __init__(self, alpha,*args, **kwargs):
         super().__init__(*args, **kwargs)
         self.alpha = alpha
