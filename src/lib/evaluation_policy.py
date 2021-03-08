5506bccc2a3b7074fa5d7298cbd7537fac145805
heitor57 <heitorwerneck@hotmail.com>
2021-03-08 18:25:22 -0300

    Pool limit in evaluation policy

--- src/lib/evaluation_policy.py
+++ src/lib/evaluation_policy.py
@@ -16,48 +16,49 @@ class Interaction(EvaluationPolicy,Parameterizable):
         self.parameters.extend(['num_interactions','interaction_size'])
 
     def evaluate(self,model,train_dataset,test_dataset):
-        test_users = np.unique(test_dataset.data[:,0]).astype(int)
-        num_total_items = test_dataset.num_total_items
-        test_consumption_matrix = scipy.sparse.csr_matrix((test_dataset.data[:,2],(test_dataset.data[:,0].astype(int),test_dataset.data[:,1].astype(int))),shape=(test_dataset.num_total_users,test_dataset.num_total_items))
+        with threadpool_limits(limits=1, user_api='blas'):
+            test_users = np.unique(test_dataset.data[:,0]).astype(int)
+            num_total_items = test_dataset.num_total_items
+            test_consumption_matrix = scipy.sparse.csr_matrix((test_dataset.data[:,2],(test_dataset.data[:,0].astype(int),test_dataset.data[:,1].astype(int))),shape=(test_dataset.num_total_users,test_dataset.num_total_items))
 
-        users_items_recommended = defaultdict(list)
-        num_test_users = len(test_users)
-        print(f"Starting {model.__class__.__name__} Training")
-        model.train(train_dataset)
-        print(f"Ended {model.__class__.__name__} Training")
-        users_num_interactions = defaultdict(int)
-        available_users = set(test_users)
+            users_items_recommended = defaultdict(list)
+            num_test_users = len(test_users)
+            print(f"Starting {model.__class__.__name__} Training")
+            model.train(train_dataset)
+            print(f"Ended {model.__class__.__name__} Training")
+            users_num_interactions = defaultdict(int)
+            available_users = set(test_users)
 
-        history_items_recommended = []
+            history_items_recommended = []
 
-        num_trials = num_test_users*self.num_interactions
-        _intervals = num_trials//20
-        _num_interactions = 0
-        pbar = tqdm(total=num_trials)
-        pbar.set_description(f"{model.__class__.__name__}")
-        for i in range(num_trials):
-            uid = random.sample(available_users,k=1)[0]
-            not_recommended = np.ones(num_total_items,dtype=bool)
-            not_recommended[users_items_recommended[uid]] = 0
-            items_not_recommended = np.nonzero(not_recommended)[0]
-            items_score, additional_data = model.predict(uid,items_not_recommended,self.interaction_size)
-            best_items = items_not_recommended[np.argpartition(items_score,-self.interaction_size)[-self.interaction_size:]]
-            users_items_recommended[uid].extend(best_items)
+            num_trials = num_test_users*self.num_interactions
+            _intervals = num_trials//20
+            _num_interactions = 0
+            pbar = tqdm(total=num_trials)
+            pbar.set_description(f"{model.__class__.__name__}")
+            for i in range(num_trials):
+                uid = random.sample(available_users,k=1)[0]
+                not_recommended = np.ones(num_total_items,dtype=bool)
+                not_recommended[users_items_recommended[uid]] = 0
+                items_not_recommended = np.nonzero(not_recommended)[0]
+                items_score, additional_data = model.predict(uid,items_not_recommended,self.interaction_size)
+                best_items = items_not_recommended[np.argpartition(items_score,-self.interaction_size)[-self.interaction_size:]]
+                users_items_recommended[uid].extend(best_items)
 
-            for item in best_items:
-                history_items_recommended.append((uid,item))
-                model.update(uid,item,test_consumption_matrix[uid,item],additional_data)
-            model.increment_time()
-            users_num_interactions[uid] += 1
-            if users_num_interactions[uid] == self.num_interactions:
-                available_users = available_users - {uid}
+                for item in best_items:
+                    history_items_recommended.append((uid,item))
+                    model.update(uid,item,test_consumption_matrix[uid,item],additional_data)
+                model.increment_time()
+                users_num_interactions[uid] += 1
+                if users_num_interactions[uid] == self.num_interactions:
+                    available_users = available_users - {uid}
 
-            _num_interactions += 1
-            if i % _intervals == 0 and i != 0:
-                pbar.update(_num_interactions)
-                _num_interactions = 0
+                _num_interactions += 1
+                if i % _intervals == 0 and i != 0:
+                    pbar.update(_num_interactions)
+                    _num_interactions = 0
 
-        pbar.update(_num_interactions)
-        _num_interactions = 0
-        pbar.close()
-        return history_items_recommended
+            pbar.update(_num_interactions)
+            _num_interactions = 0
+            pbar.close()
+            return history_items_recommended
