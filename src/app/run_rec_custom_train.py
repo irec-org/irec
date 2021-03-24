df86994d5df7e093839f7d90828cdd7d06ac401a
heitor57 <heitorwerneck@hotmail.com>
2021-03-23 23:13:28 -0300

    Custom train working

--- src/app/run_rec_custom_train.py
+++ src/app/run_rec_custom_train.py
@@ -1,34 +1,36 @@
-from os.path import dirname, realpath, sep, pardir
 import os
+from os.path import dirname, realpath, sep, pardir
 import sys
 from copy import copy
 sys.path.append(dirname(realpath(__file__)) + sep + pardir + sep + "lib")
+import argparse
+parser = argparse.ArgumentParser()
+# parser.add_argument('--forced_run', default=False, action='store_true')
+# parser.add_argument('--parallel', default=False, action='store_true')
+parser.add_argument('-m',nargs='*')
+parser.add_argument('-b',nargs='*')
+parser.add_argument('-estart',default='LimitedInteraction')
+parser.add_argument('-elast',default='Interaction')
+args = parser.parse_args()
 import inquirer
 import interactors
+from utils.InteractorRunner import InteractorRunner
+import joblib
+from utils.DatasetManager import DatasetManager
 import mf
-import util
-from util import DatasetFormatter, MetricsEvaluator
+import utils.util as util
+# from util import DatasetFormatter, MetricsEvaluator
 from sklearn.decomposition import NMF
 import numpy as np
 import scipy.sparse
-import recommenders
-import argparse
+# import recommenders
 import evaluation_policy
-parser = argparse.ArgumentParser()
-parser.add_argument('--forced_run', default=False, action='store_true')
-parser.add_argument('--parallel', default=False, action='store_true')
-parser.add_argument('-m',nargs='*')
-parser.add_argument('-b',nargs='*')
-parser.add_argument('-efirst')
-parser.add_argument('-elast')
-args = parser.parse_args()
+import yaml
+import utils.dataset as dataset
+from utils.InteractorCache import InteractorCache
+from utils.PersistentDataManager import PersistentDataManager
 
-if args.e == None:
-    first_evaluation_policy = ir.get_interactors_evaluation_policy()
-else:
-    first_evaluation_policy = eval('evaluation_policy.'+args.efirst)()
 
-last_evaluation_policy = eval('evaluation_policy.'+args.elast)()
 interactors_preprocessor_paramaters = yaml.load(
     open("settings" + sep + "interactors_preprocessor_parameters.yaml"),
     Loader=yaml.SafeLoader)
@@ -47,6 +49,9 @@ with open("settings"+sep+"datasets_preprocessors_parameters.yaml") as f:
     datasets_preprocessors = {setting['name']: setting
                               for setting in datasets_preprocessors}
 
+
+last_evaluation_policy = eval('evaluation_policy.'+args.elast)(**evaluation_policies_parameters[args.elast])
+
 dm = DatasetManager()
 datasets_preprocessors = [datasets_preprocessors[base] for base in args.b]
 ir = InteractorRunner(None, interactors_general_settings,
@@ -60,66 +65,57 @@ for dataset_preprocessor in datasets_preprocessors:
     dm.load()
     data = np.vstack(
         (dm.dataset_preprocessed[0].data, dm.dataset_preprocessed[1].data))
-    dataset = Dataset(data)
+    dataset = dataset.Dataset(data)
     dataset.update_from_data()
     dataset.update_num_total_users_items()
     consumption_matrix = scipy.sparse.csr_matrix((dataset.data[:,2],(dataset.data[:,0].astype(int),dataset.data[:,1].astype(int))),shape=(dataset.num_total_users,dataset.num_total_items))
     for history_rate in history_rates_to_train:
         print('%.2f%% of history'%(history_rate*100))
         for interactor_class in interactors_classes:
-            # ir = InteractorRunner(dm, interactors_general_settings,
-                          # interactors_preprocessor_paramaters,
-                          # evaluation_policies_parameters)
-                        # interactions=100,
-                        # interaction_size=5)
-            # ir.run_interactors(interactors_classes,forced_run=args.forced_run, parallel=args.parallel)
-            itr = interactor_class(**interactors_preprocessor_paramaters[datasets_preprocessors['name']][interactor_class.__name__])
-            users_items_recommended = evaluation_policy.evaluate(
-                itr, self.dm.dataset_preprocessed[0],
-                self.dm.dataset_preprocessed[1])
+            # print(
+            # datasets_preprocessors['name'],interactor_class.__name__)
+            itr = interactor_class(**interactors_preprocessor_paramaters[dataset_preprocessor['name']][interactor_class.__name__]['parameters'])
 
-            # interactor_model.results = interactor_model.load_results()
-            # pdm = PersistentDataManager(directory='results')
-            # users_items_recommended = pdm.load(InteractorCache().get_id(
-                # dm, first_evaluation_policy, itr))
-            new_data = []
-            for (user, item) in users_items_recommended:
-                new_data.append((user,item,consumption_matrix[user,item]))
-            new_train_data = np.array(new_data)
-            train_data = dm.dataset_preprocessed[0].data
-            new_train_data = np.vstack(
-                (train_data, new_data))
-            new_test_data=test_data[~(np.isin(test_data[:,0],set(new_train_data[:,0])) & np.isin(test_data[:,1],set(new_train_data[:,1])))]
-            train_dataset = copy(dataset)
-            train_dataset.data = new_train_data
-            train_dataset.update_from_data()
-            test_dataset = copy(dataset)
-            test_dataset.data = new_test_data
-            test_dataset.update_from_data()
+            start_evaluation_policy = eval('evaluation_policy.'+args.estart)(**evaluation_policies_parameters[args.estart])
+            start_evaluation_policy.recommend_test_data_rate_limit = history_rate
+            # no need history rate s but i will put it because of consistency
+            file_name = 's_'+str(history_rate)+'_'+InteractorCache().get_id(dm,start_evaluation_policy,itr)
 
             pdm = PersistentDataManager(directory='results')
-            # if forced_run or not pdm.file_exists(InteractorCache().get_id(
-                    # self.dm, evaluation_policy, itr)):
-            itr = interactor_class(**interactors_preprocessor_paramaters[datasets_preprocessors['name']][interactor_class.__name__])
-            history_items_recommended = last_evaluation_policy.evaluate(
-                itr, train_dataset,
-                test_dataset)
+            if not pdm.file_exists(file_name):
+                history_items_recommended = start_evaluation_policy.evaluate(
+                    itr, dm.dataset_preprocessed[0],
+                    dm.dataset_preprocessed[1])
+                pdm.save(file_name,
+                         history_items_recommended)
+            else:
+                history_items_recommended = pdm.load(file_name )
+                print("File already exists")
 
-            pdm = PersistentDataManager(directory='results')
-            pdm.save(InteractorCache().get_id(self.dm, evaluation_policy, itr),
-                     history_items_recommended)
+            itr = interactor_class(**interactors_preprocessor_paramaters[dataset_preprocessor['name']][interactor_class.__name__]['parameters'])
 
-            # users_items_recommended = evaluation_policy.evaluate(
-                # itr, self.dm.dataset_preprocessed[0],
-                # self.dm.dataset_preprocessed[1])
+            file_name = 'e_'+str(history_rate)+'_'+InteractorCache().get_id(dm,last_evaluation_policy,itr)
 
-            # data = np.vstack(
-                # (dm.dataset_preprocessed.data, dm.dataset_preprocessed[1].data))
-
-            # print('\t*',interactor_class.__name__)
-            # for recommender_class in recommenders_class:
-                # print('\t\t-',recommender_class.__name__)
-                # recommender_model = recommender_class(name_prefix=dsf.base,
-                                                      # name_suffix=interactor_model.get_id()+'_history_rate_%.2f'%(history_rate))
-                # recommender_model.train(train_matrix)
-                # recommender_model.predict(test_matrix)
+            if not pdm.file_exists(file_name):
+                new_data = []
+                for (user, item) in history_items_recommended:
+                    new_data.append((user,item,consumption_matrix[user,item]))
+                new_train_data = np.array(new_data)
+                train_data = dm.dataset_preprocessed[0].data
+                new_train_data = np.vstack(
+                        (train_data[:,0:3], new_data))
+                test_data = dm.dataset_preprocessed[1].data
+                new_test_data=test_data[~(np.isin(test_data[:,0],set(new_train_data[:,0])) & np.isin(test_data[:,1],set(new_train_data[:,1])))]
+                train_dataset = copy(dataset)
+                train_dataset.data = new_train_data
+                train_dataset.update_from_data()
+                test_dataset = copy(dataset)
+                test_dataset.data = new_test_data
+                test_dataset.update_from_data()
+                history_items_recommended = last_evaluation_policy.evaluate(
+                    itr, train_dataset,
+                    test_dataset)
+                pdm.save(file_name,
+                         history_items_recommended)
+            else:
+                print("File already exists")
--- src/lib/evaluation_policy.py
+++ src/lib/evaluation_policy.py
@@ -165,3 +165,54 @@ class InteractionSample(EvaluationPolicy,Parameterizable):
             fig.savefig(os.path.join(DirectoryDependent().DIRS["img"],f'plot_{uid}.png'),bbox_inches = 'tight')
 
         return history_items_recommended
+
+class LimitedInteraction(EvaluationPolicy,Parameterizable):
+    def __init__(self, interaction_size, recommend_test_data_rate_limit, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.interaction_size = interaction_size
+        self.recommend_test_data_rate_limit = recommend_test_data_rate_limit
+        self.parameters.extend(['interaction_size','recommend_test_data_rate_limit'])
+
+    def evaluate(self,model,train_dataset,test_dataset):
+        with threadpool_limits(limits=1, user_api='blas'):
+            test_users = np.unique(test_dataset.data[:,0]).astype(int)
+            num_total_items = test_dataset.num_total_items
+            test_consumption_matrix = scipy.sparse.csr_matrix((test_dataset.data[:,2],(test_dataset.data[:,0].astype(int),test_dataset.data[:,1].astype(int))),shape=(test_dataset.num_total_users,test_dataset.num_total_items))
+
+            users_items_recommended = defaultdict(list)
+            num_test_users = len(test_users)
+            print(f"Starting {model.__class__.__name__} Training")
+            model.train(train_dataset)
+            print(f"Ended {model.__class__.__name__} Training")
+            # users_num_interactions = defaultdict(int)
+            users_num_items_to_recommend_from_test = dict()
+            available_users = set()
+            for uid in test_users:
+                users_num_items_to_recommend_from_test[uid] = np.floor((test_consumption_matrix[uid] > 0).count_nonzero()*self.recommend_test_data_rate_limit)
+                if users_num_items_to_recommend_from_test[uid] > 0:
+                    available_users |= {uid}
+            
+            users_num_items_recommended_from_test = defaultdict(int)
+
+            history_items_recommended = []
+
+            while len(available_users) > 0:
+                uid = random.sample(available_users,k=1)[0]
+                not_recommended = np.ones(num_total_items,dtype=bool)
+                not_recommended[users_items_recommended[uid]] = 0
+                items_not_recommended = np.nonzero(not_recommended)[0]
+                items_score, additional_data = model.predict(uid,items_not_recommended,self.interaction_size)
+                best_items = items_not_recommended[np.argpartition(items_score,-self.interaction_size)[-self.interaction_size:]]
+                users_items_recommended[uid].extend(best_items)
+
+                for item in best_items:
+                    history_items_recommended.append((uid,item))
+                    model.update(uid,item,test_consumption_matrix[uid,item],additional_data)
+                    users_num_items_recommended_from_test[uid]+=test_consumption_matrix[uid,item]>0
+
+                model.increment_time()
+                # users_num_interactions[uid] += 1
+                if users_num_items_recommended_from_test[uid] >= users_num_items_to_recommend_from_test[uid]:
+                    available_users = available_users - {uid}
+
+            return history_items_recommended
