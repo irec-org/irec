163171594cb40c62f3e32b0d13f121b077a80a58
heitor57 <heitorwerneck@hotmail.com>
2021-03-24 11:50:48 -0300

    Parallel custom train

--- src/app/run_rec_custom_train.py
+++ src/app/run_rec_custom_train.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser()
 # parser.add_argument('--parallel', default=False, action='store_true')
 parser.add_argument('-m',nargs='*')
 parser.add_argument('-b',nargs='*')
+parser.add_argument('--num_tasks', type=int, default=os.cpu_count())
 parser.add_argument('-estart',default='LimitedInteraction')
 parser.add_argument('-elast',default='Interaction')
 args = parser.parse_args()
@@ -16,7 +17,9 @@ import inquirer
 import interactors
 from utils.InteractorRunner import InteractorRunner
 import joblib
+import concurrent.futures
 from utils.DatasetManager import DatasetManager
+from concurrent.futures import ProcessPoolExecutor, wait, FIRST_COMPLETED
 import mf
 import utils.util as util
 # from util import DatasetFormatter, MetricsEvaluator
@@ -50,7 +53,6 @@ with open("settings"+sep+"datasets_preprocessors_parameters.yaml") as f:
                               for setting in datasets_preprocessors}
 
 
-last_evaluation_policy = eval('evaluation_policy.'+args.elast)(**evaluation_policies_parameters[args.elast])
 
 dm = DatasetManager()
 datasets_preprocessors = [datasets_preprocessors[base] for base in args.b]
@@ -60,62 +62,78 @@ ir = InteractorRunner(None, interactors_general_settings,
 interactors_classes = [eval('interactors.'+interactor) for interactor in args.m]
 history_rates_to_train = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]
 
-for dataset_preprocessor in datasets_preprocessors:
-    dm.initialize_engines(dataset_preprocessor)
-    dm.load()
-    data = np.vstack(
-        (dm.dataset_preprocessed[0].data, dm.dataset_preprocessed[1].data))
-    dataset = dataset.Dataset(data)
-    dataset.update_from_data()
-    dataset.update_num_total_users_items()
-    consumption_matrix = scipy.sparse.csr_matrix((dataset.data[:,2],(dataset.data[:,0].astype(int),dataset.data[:,1].astype(int))),shape=(dataset.num_total_users,dataset.num_total_items))
-    for history_rate in history_rates_to_train:
-        print('%.2f%% of history'%(history_rate*100))
-        for interactor_class in interactors_classes:
-            # print(
-            # datasets_preprocessors['name'],interactor_class.__name__)
-            itr = interactor_class(**interactors_preprocessor_paramaters[dataset_preprocessor['name']][interactor_class.__name__]['parameters'])
+def process():
+    itr = interactor_class(**interactors_preprocessor_paramaters[dataset_preprocessor['name']][interactor_class.__name__]['parameters'])
 
-            start_evaluation_policy = eval('evaluation_policy.'+args.estart)(**evaluation_policies_parameters[args.estart])
-            start_evaluation_policy.recommend_test_data_rate_limit = history_rate
-            # no need history rate s but i will put it because of consistency
-            file_name = 's_'+str(history_rate)+'_'+InteractorCache().get_id(dm,start_evaluation_policy,itr)
+    start_evaluation_policy = eval('evaluation_policy.'+args.estart)(**evaluation_policies_parameters[args.estart])
+    start_evaluation_policy.recommend_test_data_rate_limit = history_rate
+    # no need history rate s but i will put it because of consistency
+    file_name = 's_'+str(history_rate)+'_'+InteractorCache().get_id(dm,start_evaluation_policy,itr)
 
-            pdm = PersistentDataManager(directory='results')
-            if not pdm.file_exists(file_name):
-                history_items_recommended = start_evaluation_policy.evaluate(
-                    itr, dm.dataset_preprocessed[0],
-                    dm.dataset_preprocessed[1])
-                pdm.save(file_name,
-                         history_items_recommended)
-            else:
-                history_items_recommended = pdm.load(file_name )
-                print("File already exists")
+    pdm = PersistentDataManager(directory='results')
+    if not pdm.file_exists(file_name):
+        history_items_recommended = start_evaluation_policy.evaluate(
+            itr, dm.dataset_preprocessed[0],
+            dm.dataset_preprocessed[1])
+        pdm.save(file_name,
+                 history_items_recommended)
+    else:
+        history_items_recommended = pdm.load(file_name )
+        print("File already exists")
 
-            itr = interactor_class(**interactors_preprocessor_paramaters[dataset_preprocessor['name']][interactor_class.__name__]['parameters'])
+    itr = interactor_class(**interactors_preprocessor_paramaters[dataset_preprocessor['name']][interactor_class.__name__]['parameters'])
 
-            file_name = 'e_'+str(history_rate)+'_'+InteractorCache().get_id(dm,last_evaluation_policy,itr)
+    last_evaluation_policy = eval('evaluation_policy.'+args.elast)(**evaluation_policies_parameters[args.elast])
+    file_name = 'e_'+str(history_rate)+'_'+InteractorCache().get_id(dm,last_evaluation_policy,itr)
 
-            if not pdm.file_exists(file_name):
-                new_data = []
-                for (user, item) in history_items_recommended:
-                    new_data.append((user,item,consumption_matrix[user,item]))
-                new_train_data = np.array(new_data)
-                train_data = dm.dataset_preprocessed[0].data
-                new_train_data = np.vstack(
-                        (train_data[:,0:3], new_data))
-                test_data = dm.dataset_preprocessed[1].data
-                new_test_data=test_data[~(np.isin(test_data[:,0],set(new_train_data[:,0])) & np.isin(test_data[:,1],set(new_train_data[:,1])))]
-                train_dataset = copy(dataset)
-                train_dataset.data = new_train_data
-                train_dataset.update_from_data()
-                test_dataset = copy(dataset)
-                test_dataset.data = new_test_data
-                test_dataset.update_from_data()
-                history_items_recommended = last_evaluation_policy.evaluate(
-                    itr, train_dataset,
-                    test_dataset)
-                pdm.save(file_name,
-                         history_items_recommended)
-            else:
-                print("File already exists")
+    if not pdm.file_exists(file_name):
+        new_data = []
+        for (user, item) in history_items_recommended:
+            new_data.append((user,item,consumption_matrix[user,item]))
+        new_train_data = np.array(new_data)
+        train_data = dm.dataset_preprocessed[0].data
+        new_train_data = np.vstack(
+                (train_data[:,0:3], new_data))
+        test_data = dm.dataset_preprocessed[1].data
+        new_test_data=test_data[~(np.isin(test_data[:,0],set(new_train_data[:,0])) & np.isin(test_data[:,1],set(new_train_data[:,1])))]
+        train_dataset = copy(dataset)
+        train_dataset.data = new_train_data
+        train_dataset.update_from_data()
+        test_dataset = copy(dataset)
+        test_dataset.data = new_test_data
+        test_dataset.update_from_data()
+        history_items_recommended = last_evaluation_policy.evaluate(
+            itr, train_dataset,
+            test_dataset)
+        pdm.save(file_name,
+                 history_items_recommended)
+    else:
+        print("File already exists")
+
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    futures = set()
+    for dataset_preprocessor in datasets_preprocessors:
+        dm.initialize_engines(dataset_preprocessor)
+        dm.load()
+        data = np.vstack(
+            (dm.dataset_preprocessed[0].data, dm.dataset_preprocessed[1].data))
+        dataset = dataset.Dataset(data)
+        dataset.update_from_data()
+        dataset.update_num_total_users_items()
+        consumption_matrix = scipy.sparse.csr_matrix((dataset.data[:,2],(dataset.data[:,0].astype(int),dataset.data[:,1].astype(int))),shape=(dataset.num_total_users,dataset.num_total_items))
+        for history_rate in history_rates_to_train:
+            print('%.2f%% of history'%(history_rate*100))
+            for interactor_class in interactors_classes:
+
+                executor.submit(process)
+                futures.add(f)
+
+                if len(futures) >= args.num_tasks:
+                    completed, futures = wait(futures, return_when=FIRST_COMPLETED)
+    for f in futures:
+        f.result()
+
+        # for f in futures:
+            # f.result()
+
