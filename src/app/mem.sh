97aa6ca8951954f0d0bbb69fcf370830a5bf31db
heitor57 <heitorwerneck@hotmail.com>
2021-04-18 23:07:53 -0300

    Memory monitor

---
+++ src/app/mem.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+while true; do 
+
+				ramusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f\n"), $3/$2*100}'| awk '{print $3}')
+
+				echo "Memory Current Usage is: $ramusage%"
+				if (( $(echo "$ramusage > 92" |bc -l) )); then
+								#SUBJECT="ATTENTION: Memory Utilization is High on $(hostname) at $(date)"
+								#MESSAGE="/tmp/Mail.out"
+								#TO="daygeek@gmail.com"
+
+								killall python3.6
+					#			break
+				fi
+				sleep 0.4
+done
