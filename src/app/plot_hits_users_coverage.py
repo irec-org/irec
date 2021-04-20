38c92ff3a103b935fb6629078c1b774221bb40ac
thiagodks <thiagoadriano2010@gmail.com>
2021-04-20 15:28:25 -0300

    update plot users coverage

--- src/app/plot_hits_users_coverage.py
+++ src/app/plot_hits_users_coverage.py
@@ -153,6 +153,8 @@ def plot_hits_users_coverage(methods_users_hits, title="Users Coverage x Hits"):
 
     ax1.legend(ncol=1)
     ax1.tick_params(labelsize=18)
+    plt.xticks(list(range(0, len(hits)+10, 10)))
+    plt.yticks(list(range(0, len(hits)+10, 10)))
 
     return fig
 
