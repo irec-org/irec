1db0e095039166e343e3c5512610b2ebedbf9351
heitor57 <heitorwerneck@hotmail.com>
2021-04-27 19:29:07 -0300

    ICTR update

--- src/lib/interactors/ICTR.py
+++ src/lib/interactors/ICTR.py
@@ -22,9 +22,14 @@ def _softmax(x):
 
 class _Particle:
     def __init__(self,num_users,num_items,num_lat):
+        self.num_users = num_users
+        self.num_items = num_items
+        self.num_lat = num_lat
         self.alpha = 1
         self.beta = 1
-        self.lambda_ = np.ones(shape=(num_users,num_lat))
+        # self.lambda_ = np.ones(shape=(num_users,num_lat))
+        # self.lambda_ = np.ones(shape=(num_lat,num_users))
+        self.lambda_ = np.ones(shape=(num_lat))
         self.eta = np.ones(shape=(num_lat,num_items))
         self.mu = np.ones(shape=(num_items,num_lat))
         # self.Sigma = np.zeros(shape=(num_items,num_lat,num_lat))
@@ -35,28 +40,30 @@ class _Particle:
         self.Phi = np.zeros(shape=(num_lat,num_items))
     # def p_expectations(self,uid,topic=None,reward=None):
     def p_expectations(self,uid,reward=None):
-        computed_sum = np.sum(self.lambda_[uid])
-        user_lambda = np.copy(self.lambda_[uid])
+        computed_sum = np.sum(self.lambda_)
+        user_lambda = np.copy(self.lambda_)
         if reward!=None:
             # user_lambda[topic] += reward
             user_lambda += reward
             computed_sum += reward
         return user_lambda/computed_sum
-    def Phi_expectations(self,item,reward=None):
+    def Phi_expectations(self,item,topic=None,reward=None):
+        # eta = np.copy(self.eta)
         computed_sum = np.sum(self.eta,axis=1)
         item_eta =self.eta[:,item]
         if reward!=None:
-            item_eta += reward
+            item_eta[topic] += reward
             computed_sum += reward
         return item_eta/computed_sum
     def particle_weight(self,uid,item,reward):
         # print(self.p[uid],self.q[item])
         norm_val = scipy.stats.norm(self.p[uid]@self.q[item],self.sigma_n_2).pdf(reward)
         return np.sum(norm_val*self.p_expectations(uid) * self.Phi_expectations(item))
-    def compute_theta(self,uid,item,reward):
-        return self.p_expectations(uid,reward=reward)*self.Phi_expectations(item,reward=reward)
+    def compute_theta(self,uid,item,reward,topic):
+        return self.p_expectations(uid,reward=reward)*self.Phi_expectations(item,reward=reward,topic=topic)
     def select_z_topic(self,uid,item,reward):
-        theta = self.compute_theta(uid,item,reward)
+        topic = np.argmax(np.random.multinomial(1,self.p[uid]))
+        theta = self.compute_theta(uid,item,reward,topic)
         theta = _softmax(theta)
         # print(np.random.multinomial(1,theta))
         topic = np.argmax(np.random.multinomial(1,theta))
@@ -70,19 +77,20 @@ class _Particle:
         new_beta = self.beta + 1/2*(
                 self.mu[item].T @ np.linalg.inv(self.Sigma[item]) @ self.mu[item]+reward*reward - new_mu.T@np.linalg.inv(new_Sigma)@new_mu
                 )
+        # new_lambda_k = self.lambda_[topic]+reward
         new_lambda_k = self.lambda_[topic]+reward
-        new_eta_k = self.eta[topic,item]+reward
+        new_eta_k = self.eta[:,item]+reward
 
         self.Sigma[item] = new_Sigma
         self.mu[item] = new_mu
         self.alpha = new_alpha
         self.beta = new_beta
         self.lambda_[topic] = new_lambda_k
-        self.eta[topic,item] = new_eta_k
+        self.eta[:,item] = new_eta_k
     def sample_random_variables(self,uid,item,topic):
         self.sigma_n_2 = scipy.stats.invgamma(self.alpha,self.beta).rvs()
         self.q[item] = np.random.multivariate_normal(self.mu[item],self.sigma_n_2*self.Sigma[item])
-        self.p[uid] = np.random.dirichlet(self.lambda_[uid])
+        self.p[uid] = np.random.dirichlet(self.lambda_)
         self.Phi[topic] = np.random.dirichlet(self.eta[topic])
 
 class ICTRTS(MFInteractor):
