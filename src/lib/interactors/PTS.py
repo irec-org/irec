fdc07b2f60d8985b5117ae8ea174cbef4efa7b2a
heitor57 <heitorwerneck@hotmail.com>
2021-05-08 20:10:50 -0300

    PTS with initialization

--- src/lib/interactors/PTS.py
+++ src/lib/interactors/PTS.py
@@ -13,6 +13,7 @@ from numba import njit, jit
 import mf
 from utils.PersistentDataManager import PersistentDataManager
 import joblib
+import interactors
 
 @njit
 def _softmax(x):
@@ -157,3 +158,41 @@ class PTS(MFInteractor):
             self.particles_vs=new_particles_vs
             self.particles_var_us=new_particles_var_us
             self.particles_var_vs=new_particles_var_vs
+
+
+class PTSInit(PTS):
+    def __init__(self, init,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.init = init
+        self.parameters.extend(['init'])
+    def train(self,train_dataset):
+        super().train(train_dataset)
+
+        if self.init == 'entropy':
+            items_entropy = interactors.Entropy.get_items_entropy(self.train_consumption_matrix)
+            self.items_bias = items_entropy
+        elif self.init == 'logpopent':
+            items_entropy = interactors.Entropy.get_items_entropy(self.train_consumption_matrix)
+            items_popularity = interactors.MostPopular.get_items_popularity(self.train_consumption_matrix,normalize=False)
+            self.items_bias = interactors.LogPopEnt.get_items_logpopent(items_popularity,items_entropy)
+
+        self.items_bias = self.items_bias - np.min(self.items_bias)
+        self.items_bias = self.items_bias/np.max(self.items_bias)
+        assert(self.items_bias.min() >= 0 and np.isclose(self.items_bias.max(), 1))
+        res=scipy.optimize.minimize(lambda x,items_means,items_bias: np.sum((items_bias - x @ items_means.T)**2),
+                                    np.ones(self.num_lat),
+                                    args=(self.particles_vs[0],self.items_bias),
+                                    method='BFGS',
+                                    )
+        self.initial_user_factors = res.x 
+        # print(np.corrcoef(self.items_bias,self.initial_user_factors @ self.items_means.T)[0,1])
+        # self.bs = defaultdict(lambda: self.initial_b.copy())
+        not_train_users = set(list(range(self.train_dataset.num_total_users))) - \
+                {int(self.train_dataset.data[i,0]) for i in range(len(self.train_dataset.data))}
+        for i in range(self.num_particles):
+            for uid in not_train_users:
+                self.particles_us[i][uid] = self.initial_user_factors
+            # self.particles_vs[i] 
+class PTSLogPopEnt(PTSInit):
+    def __init__(self,*args, **kwargs):
+        super().__init__(init='logpopent',*args, **kwargs)
