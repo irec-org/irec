443a8ae894aa4b0ea92c83213ec95e4613e01b10
heitor57 <heitorwerneck@hotmail.com>
2021-05-26 21:38:58 -0300

    One interaction evaluator

--- src/lib/evaluation_policies.py
+++ src/lib/evaluation_policies.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+
 from threadpoolctl import threadpool_limits
 import scipy.sparse
 from lib.utils.Parameterizable import Parameterizable
@@ -223,4 +224,90 @@ class LimitedInteraction(EvaluationPolicy,Parameterizable):
                 if users_num_items_recommended_from_test[uid] >= users_num_items_to_recommend_from_test[uid]:
                     available_users = available_users - {uid}
 
+            return history_items_recommended
+
+
+class OneInteraction(EvaluationPolicy,Parameterizable):
+    def __init__(self, num_interactions, interaction_size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.num_interactions = num_interactions
+        self.interaction_size = interaction_size
+        self.parameters.extend(['num_interactions','interaction_size'])
+
+    def evaluate(self,model,train_dataset,test_dataset):
+        with threadpool_limits(limits=1, user_api='blas'):
+            test_users = np.unique(test_dataset.data[:,0]).astype(int)
+            num_total_items = test_dataset.num_total_items
+            test_consumption_matrix = scipy.sparse.csr_matrix((test_dataset.data[:,2],(test_dataset.data[:,0].astype(int),test_dataset.data[:,1].astype(int))),shape=(test_dataset.num_total_users,test_dataset.num_total_items))
+
+            users_items_recommended = defaultdict(list)
+
+            for i in range(len(train_dataset.data)):
+                uid = int(train_dataset.data[i,0])
+                if uid in test_users:
+                    iid = int(train_dataset.data[i,1])
+                    reward = train_dataset.data[i,2]
+                    users_items_recommended[uid].append(iid)
+
+            num_test_users = len(test_users)
+            print(f"Starting {model.__class__.__name__} Training")
+            model.train(train_dataset)
+            print(f"Ended {model.__class__.__name__} Training")
+            users_num_interactions = defaultdict(int)
+            available_users = set(test_users)
+
+            history_items_recommended = []
+
+            num_trials = num_test_users*self.num_interactions
+            _intervals = num_trials//20
+            _num_interactions = 0
+            pbar = tqdm(total=num_trials)
+            pbar.set_description(f"{model.__class__.__name__}")
+            no_items_recommended_users = set(test_users)
+
+            train_consumption_matrix = scipy.sparse.csr_matrix((train_dataset.data[:,2],(train_dataset.data[:,0],train_dataset.data[:,1])),(train_dataset.num_total_users,train_dataset.num_total_items))
+
+            items_entropy = lib.interactors.Entropy.get_items_entropy(train_consumption_matrix)
+            items_popularity = lib.interactors.MostPopular.get_items_popularity(train_consumption_matrix,normalize=False)
+            del train_consumption_matrix
+            correlations = defaultdict(dict)
+            for i in range(num_trials):
+                uid = random.sample(available_users,k=1)[0]
+                not_recommended = np.ones(num_total_items,dtype=bool)
+                not_recommended[users_items_recommended[uid]] = 0
+                items_not_recommended = np.nonzero(not_recommended)[0]
+                items_score, additional_data = model.predict(uid,items_not_recommended,self.interaction_size)
+                best_items = items_not_recommended[np.argpartition(items_score,-self.interaction_size)[-self.interaction_size:]]
+                users_items_recommended[uid].extend(best_items)
+
+                for item in best_items:
+                    history_items_recommended.append((uid,item))
+                    model.update(uid,item,test_consumption_matrix[uid,item],additional_data)
+                model.increment_time()
+
+
+                users_num_interactions[uid] += 1
+                if users_num_interactions[uid] == self.num_interactions:
+                    available_users = available_users - {uid}
+
+                if users_num_interactions[uid] == 1:
+                    correlations['Popularity'][uid]= scipy.stats.pearsonr(items_score,items_popularity)[0]
+                    correlations['Entropy'][uid]= scipy.stats.pearsonr(items_score,items_entropy)[0]
+                    no_items_recommended_users -= {uid}
+
+                _num_interactions += 1
+                if i % _intervals == 0 and i != 0:
+                    pbar.update(_num_interactions)
+                    _num_interactions = 0
+
+                if len(no_items_recommended_users) == 0:
+                    print(f"Finished recommendations in {i+1} trial")
+                    print('Correlation: {} {}'.format(model.__class__.__name__,
+                        ' '.join(['{} {}'.format(corr_name,np.mean(list(corr_values.values()))) for corr_name, corr_values in correlations.items() ])))
+                    break
+            
+
+            pbar.update(_num_interactions)
+            _num_interactions = 0
+            pbar.close()
             return history_items_recommended
