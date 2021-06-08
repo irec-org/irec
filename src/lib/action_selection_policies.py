002a26db49784f97b35e006bb4777eaa6dd346eb
heitor57 <heitorwerneck@hotmail.com>
2021-06-07 23:38:29 -0300

    Basic action selection policies

---
+++ src/lib/action_selection_policies.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+
+class ActionSelectionPolicy:
+
+    def __init__(self):
+        pass
+
+    def select_actions(action_estimates, actions_num):
+        raise NotImplementedError
+
+    def update(self, observation, action, reward, info):
+        raise NotImplementedError
+
+    def reset(self, observation):
+        raise NotImplementedError
+
+
+class Greedy(ActionSelectionPolicy):
+
+    def select_actions(action_estimates, actions_num):
+        return np.argpartition(action_estimates,
+                               -self.actions_num)[-self.actions_num:], None
+
+    def update(self, observation, action, reward, info):
+        pass
+
+    def reset(self, observation):
+        pass
+
+
+class eGreedy(ActionSelectionPolicy):
+
+    def __init__(self,epsilon, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self.epsilon = epsilon
+
+    def select_actions(action_estimates, actions_num):
+        greedy_actions= np.argpartition(action_estimates,
+                -self.actions_num)[-self.actions_num:][::-1]
+        actions_indexes =[]
+        for i in range(actions_num):
+            if self.epsilon < np.random.rand():
+                j=0
+                while True:
+                    action_index = greedy_actions[j]
+                    if action_index not in actions_indexes:
+                        break
+                    j+=1
+                actions_indexes.append(action_index)
+            else:
+                while True:
+                    action_index = np.random.rand(len(candidate_items))
+                    if action_index not in actions_indexes:
+                        break
+                actions_indexes.append(action_index)
+
+        return np.argpartition(action_estimates,
+                               -self.actions_num)[-self.actions_num:], None
+
+    def update(self, observation, action, reward, info):
+        pass
+
+    def reset(self, observation):
+        pass
---
+++ src/lib/agents.py
@@ -0,0 +1,50 @@
+class DotDict(dict):
+
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self.__dict__ = self
+
+
+class Agent:
+
+    def __init__(self, value_function, action_selection_policy):
+        self.value_function = value_function
+        self.action_selection_policy = action_selection_policy
+        self.state = DotDict()
+
+    def act(self):
+        raise NotImplementedError
+
+    def observe(self, observation, action, reward, done):
+        raise NotImplementedError
+
+    def reset(self, observation):
+        raise NotImplementedError
+
+
+class SimpleAgent:
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def act(self, candidate_actions, actions_num):
+        action_estimates, vf_info = self.value_function.action_estimates(
+            candidate_actions)
+        actions_indexes, asp_info = self.action_selection_policy.select_actions(
+            action_estimates, actions_num)
+        actions = candidate_actions[actions_indexes]
+        return actions, {'vf_info': vf_info, 'asp_info': asp_info}
+
+    def observe(self, observation, action, reward, done, info):
+        self.value_function.update(observation, action, reward, info['vf_info'])
+        self.action_selection_policy.update(observation, action, reward,
+                                            info['asp_info'])
+        return None
+
+    def reset(self, observation):
+        if observation != None:
+            self.value_function.reset(observation)
+            self.action_selection_policy.reset(observation)
+            return None
+        else:
+            raise NotImplementedError
