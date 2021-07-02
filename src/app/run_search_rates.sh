31e34f8fbdc6d8a5baefa41cf8a8945208a47d71
heitor57 <heitorwerneck@hotmail.com>
2021-07-02 16:15:25 -0300

    helper fix

--- src/app/run_search_rates.sh
+++ src/app/run_search_rates.sh
@@ -1,4 +1,4 @@
 for i in $(seq 0.1 0.1 0.5);
 do
-	python3 run.py -b "MovieLens 10M" "Good Books" "Yahoo Music" -m Random MostPopular UCB ThompsonSampling LinearEGreedy LinearUCB GLM_UCB OurMethod2 --num_tasks 2
+	python3 run.py -b "MovieLens 10M" "Good Books" "Yahoo Music" -m Random MostPopular UCB ThompsonSampling LinearEGreedy LinearUCB GLM_UCB OurMethod2 --num_tasks 2 --evaluation_policies_parameters.LimitedInteraction.recommend_test_data_rate_limit=$i
 done
