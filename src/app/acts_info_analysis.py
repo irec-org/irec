8420fe3dc70993ce4135071478b65e9084c6e407
heitor57 <heitorwerneck@hotmail.com>
2021-07-05 17:00:12 -0300

    parameters and execution save

--- src/app/acts_info_analysis.py
+++ src/app/acts_info_analysis.py
@@ -20,6 +20,7 @@ import inquirer
 import lib.value_functions
 import lib.mf
 from lib.utils.InteractorRunner import InteractorRunner
+import lib.agents
 from sklearn.decomposition import NMF
 import numpy as np
 import scipy.sparse
@@ -40,6 +41,7 @@ ir = InteractorRunner(dm, settings['interactors_general_settings'],
                       settings['agents_preprocessor_parameters'],
                       settings['evaluation_policies_parameters'])
 # for dataset_preprocessor in datasets_preprocessors:
+
 for base in args.b:
     dataset_preprocessor = settings['datasets_preprocessors_parameters'][base]
     dm.initialize_engines(dataset_preprocessor)
@@ -62,13 +64,14 @@ for base in args.b:
     evaluation_policy = eval('lib.evaluation_policies.' +
                              evaluation_policy_name)(
                                  **evaluation_policy_parameters)
+    # all_data= []
 
     for agent_name in args.m:
         parameters = settings['agents_preprocessor_parameters'][base][
             agent_name]
         agent = utils.create_agent(agent_name, parameters)
         agent_id = utils.get_agent_id(agent_name, parameters)
-        agent_methods= ','.join(map(lambda x: list(x.keys())[0], list(list(parameters.values())[0]['agents'])))
+        # agent_methods= ','.join(map(lambda x: list(x.keys())[0], list(list(parameters.values())[0]['agents'])))
         pdm = PersistentDataManager(directory='results')
         users_items_recommended = pdm.load(
             utils.get_experiment_run_id(dm, evaluation_policy, agent_id))
@@ -81,18 +84,42 @@ for base in args.b:
         for i in range(len(acts_info)):
             uid = users_items_recommended[i][0]
             iid = users_items_recommended[i][1]
-            data.append({
-                **acts_info[i],
-                **{
-                    'uid': uid,
-                    'iid': iid,
-                    'reward': consumption_matrix[uid, iid]
-                }
-            })
-        df_results = pd.DataFrame(data)
-        results = df_results.groupby(['user_interaction', 'meta_action_name'])['trial'].agg(['count'])
-        print(df_results.head())
-        print(results)
-
-        results.to_csv(f'{dd.DIRS["export"]}/{base}_{agent_name}_{agent_methods}.csv')
-        print(df_results.groupby('meta_action_name')['reward'].mean())
+            if isinstance(agent,lib.agents.SimpleEnsembleAgent):
+                data.append({
+                    **acts_info[i],
+                    **{
+                        'uid': uid,
+                        'iid': iid,
+                        'reward': consumption_matrix[uid, iid]
+                    }
+                })
+            else:
+                data.append(
+                        [uid,
+                        iid,
+                        consumption_matrix[uid, iid],]
+                        # i,]
+                        # agent_id,]
+                        # base,]
+                )
+                # data.append({
+                    # **{
+                        # 'uid': uid,
+                        # 'iid': iid,
+                        # 'reward': consumption_matrix[uid, iid],
+                        # 'trial': i,
+                        # 'name': agent_id,
+                        # 'base': base,
+                    # }
+                # })
+        # all_data.extend(data)
+        # print(df_results.head())
+        if isinstance(agent,lib.agents.SimpleEnsembleAgent):
+            df_results = pd.DataFrame(data)
+            results = df_results.groupby(['user_interaction', 'meta_action_name'])['trial'].agg(['count'])
+            print(results)
+            results.to_csv(f'{dd.DIRS["export"]}/{base}_{agent_name}_{agent_methods}.csv')
+            print(df_results.groupby('meta_action_name')['reward'].mean())
+        # df_all_results=pd.DataFrame(data,columns=['uid','iid','reward','trial','name'])
+        df_all_results=pd.DataFrame(data,columns=['uid','iid','reward'])
+        df_all_results.to_parquet(open(f'{dd.DIRS["export"]}/d_{base}_{agent_name}.parquet','wb'))
