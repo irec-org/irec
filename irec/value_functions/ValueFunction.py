8f26bf49c1b6f4709cc983410bf6ecca3c02153f
heitor57 <heitorwerneck@hotmail.com>
2021-09-22 15:08:37 -0300

    docs: value function

--- irec/value_functions/ValueFunction.py
+++ irec/value_functions/ValueFunction.py
@@ -12,25 +12,57 @@ from collections import defaultdict
 import pickle
 import json
 
-from typing import Any
+from typing import Any, Tuple
 
 
 class ValueFunction:
+    """ValueFunction."""
+
     def __init__(self, *args, **kwargs):
+        """__init__.
+
+        Args:
+            args:
+            kwargs:
+        """
         super().__init__(*args, **kwargs)
 
     def reset(self, observation: Any):
+        """reset.
+
+        Args:
+            observation (Any): observation
+        """
         train_dataset = observation
         pass
         # super().reset(train_dataset)
 
-    def action_estimates(self, candidate_actions: CandidateActions):
+    def action_estimates(self, candidate_actions: CandidateActions) -> Tuple[Any, dict]:
+        """action_estimates.
+
+        Args:
+            candidate_actions (CandidateActions): candidate_actions
+
+        Returns:
+            Tuple[Any, dict]: Scores and information
+        """
+
         # uid = candidate_actions[0]
         # candidate_items = candidate_actions[1]
         return None, None
 
-    def update(self, observation, action: CandidateAction, reward: int, info: dict):
+    def update(
+        self, observation: Any, action: CandidateAction, reward: float, info: dict
+    ) -> None:
+        """update.
+
+        Args:
+            observation (Any): observation
+            action (CandidateAction): action
+            reward (float): reward
+            info (dict): info
+        """
         # uid = action[0]
         # item = action[1]
         # additional_data = info
-        pass
+        return None
