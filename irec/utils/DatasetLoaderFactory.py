b72d31ad364d1f7ca152587940f75111c4a43b5b
Thiago Silva <48692251+thiagodks@users.noreply.github.com>
2021-11-01 18:59:56 -0300

    Update DatasetLoaderFactory.py
    add yahoo music r1

--- irec/utils/DatasetLoaderFactory.py
+++ irec/utils/DatasetLoaderFactory.py
@@ -13,18 +13,15 @@ class DatasetLoaderFactory:
             "MovieLens 20M",
             "LastFM 5k",
             "Kindle Store",
-            "Kindle 4k",
+            "Kindle Store 4k",
             "Netflix 10k",
             "Good Books",
             "Yahoo Music",
             "Good Reads 10k",
+            "Yahoo Music 5k",
         ]:
             dl = DatasetLoader.DefaultDatasetLoader(
                 dataset.DefaultDataset(), **dataset_parameters
-            )
-        elif dataset_name == "Netflix":
-            dl = DatasetLoader.DefaultDatasetLoader(
-                dataset.Netflix(), **dataset_parameters
             )
         elif dataset_name in [
             "MovieLens 1M Validation",
@@ -32,16 +29,20 @@ class DatasetLoaderFactory:
             "MovieLens 20M Validation",
             "LastFM 5k Validation",
             "Kindle Store Validation",
-            "Kindle 4k Validation",
+            "Kindle Store 4k Validation",
             "Good Books Validation",
             "Yahoo Music Validation",
             "Netflix 10k Validation",
             "Good Reads 10k Validation",
+            "Yahoo Music 5k Validation",
         ]:
             dl = DatasetLoader.DefaultValidationDatasetLoader(
                 dataset.DefaultDataset(), **dataset_parameters
             )
-
+        elif dataset_name == "Netflix":
+            dl = DatasetLoader.DefaultDatasetLoader(
+                dataset.Netflix(), **dataset_parameters
+            )
         elif dataset_name in ["Netflix Validation"]:
             dl = DatasetLoader.DefaultDatasetLoader(
                 dataset.Netflix(), **dataset_parameters
@@ -51,6 +52,7 @@ class DatasetLoaderFactory:
             "Good Books TRTE",
             "LastFM 5k TRTE",
             "MovieLens 10M TRTE",
+            "MovieLens 100k TRTE",
             "Yahoo Music TRTE",
             "GoodBooks TRTE",
             "LastFM 5k TRTE",
