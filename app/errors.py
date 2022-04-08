1d81bccf00d20658ec29ce9eeaa9dbc4358ca49b
heitor57 <heitorwerneck@hotmail.com>
2022-04-08 18:44:18 -0300

    refactor(app): removed application files

--- app/errors.py
+++
@@ -1,7 +0,0 @@
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-
-
-class EvaluationRunNotFoundError(Error):
-    pass
--- app/output_helpers/output_search_helper.sh
+++
@@ -1,4 +0,0 @@
-sort -k10 output_search.txt | awk 'match($0, /lambda_:[0-9]*\.?[0-9]*/) {
-    print substr($0, RSTART+8, RLENGTH-8), $0
-}
-' |  sort -k8,8 -k11n -k1n > output_search_normalized.txt
