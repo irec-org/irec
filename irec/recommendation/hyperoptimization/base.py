13022ed5147dd8f83de7d33299739ae943fdfa76
thiagodks <thiagoadriano2010@gmail.com>
2022-04-25 21:09:26 -0300

    add execute in Tunning

--- irec/recommendation/hyperoptimization/base.py
+++ irec/recommendation/hyperoptimization/base.py
@@ -1,7 +1,8 @@
 from typing import Dict
 from irec.environment.dataset import Dataset
 from concurrent.futures import ProcessPoolExecutor, wait, FIRST_COMPLETED
-
+from irec.connector.utils import run_agent
+import copy
 
 class Tunning:
     """docstring for Tunning"""
@@ -12,25 +13,20 @@ class Tunning:
     def generate_settings(self) -> Dict:
         pass
 
-    # def execute(self, x_validation:Dataset, y_validation:Dataset, agents_search_parameters, tasks, forced_run):
-    #     # def run_agent_search(
-    # # agents, dataset_loaders, settings, agents_search_parameters, tasks, forced_run
-    #     with ProcessPoolExecutor(max_workers=tasks) as executor:
-    #         futures = set()
-    #         for dataset_loader_name in dataset_loaders:
-    #             settings["defaults"]["dataset_loader"] = dataset_loader_name
-    #             data = load_dataset_experiment(settings)
-    #             for agent_name in agents:
-    #                 settings["defaults"]["agent"] = agent_name
-    #                 for agent_og_parameters in agents_search_parameters[agent_name]:
-    #                     settings["agents"][agent_name] = agent_og_parameters
-    #                     f = executor.submit(
-    #                         run_agent, data, copy.deepcopy(settings), forced_run
-    #                     )
-    #                     futures.add(f)
-    #                     if len(futures) >= tasks:
-    #                         completed, futures = wait(futures, return_when=FIRST_COMPLETED)
-    #         for f in futures:
-    #             f.result()
-
-
+    def execute(self, x_validation:Dataset, y_validation:Dataset, agents_search_parameters, settings, tasks, forced_run):
+        
+        with ProcessPoolExecutor(max_workers=tasks) as executor:
+            futures = set()
+            for agent_name in agents_search_parameters:
+                settings["defaults"]["agent"] = agent_name
+                for agent_og_parameters in agents_search_parameters[agent_name]:
+                    settings["agents"][agent_name] = agent_og_parameters
+                    f = executor.submit(
+                        # run_agent, train_dataset, test_dataset, copy.deepcopy(settings), forced_run
+                        run_agent, x_validation, y_validation, copy.deepcopy(settings), forced_run
+                    )
+                    futures.add(f)
+                    if len(futures) >= tasks:
+                        completed, futures = wait(futures, return_when=FIRST_COMPLETED)
+            for f in futures:
+                f.result()
