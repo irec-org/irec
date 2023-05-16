76bc70507b0ab3ff0a8c01baab65093c3e78525c
thiagodks <thiagoadriano2010@gmail.com>
2023-05-15 22:41:40 -0300

    add contextual value functions

---
+++ irec/recommendation/agents/value_functions/contextual_linear_egreedy.py
@@ -0,0 +1,44 @@
+from irec.recommendation.agents.value_functions import LinearEGreedy
+from irec.recommendation.agents.value_functions import MostPopular
+from irec.recommendation.agents.value_functions.log_pop_ent import LogPopEnt
+from irec.recommendation.agents.value_functions import Entropy
+from collections import defaultdict
+import numpy as np
+import scipy
+
+
+class ContextualEgreedy(LinearEGreedy):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def reset(self, observation):
+        train_dataset = observation
+        super().reset(train_dataset)
+
+        items_entropy = Entropy.get_items_entropy(
+            self.train_consumption_matrix)
+        items_popularity = MostPopular.get_items_popularity(
+            self.train_consumption_matrix, normalize=False)
+        self.items_bias = LogPopEnt.get_items_logpopent(
+            items_popularity, items_entropy)
+
+        self.items_bias = self.items_bias - np.min(self.items_bias)
+        self.items_bias = self.items_bias / np.max(self.items_bias)
+
+        assert (self.items_bias.min() >= 0
+                and np.isclose(self.items_bias.max(), 1))
+
+        res = scipy.optimize.minimize(
+            lambda x, items_means, items_bias: np.sum(
+                (items_bias - x @ items_means.T)**2),
+            np.ones(self.num_latent_factors),
+            args=(self.items_means, self.items_bias),
+            method='BFGS',
+        )
+        self.initial_b = res.x
+
+        print(
+            np.corrcoef(self.items_bias,
+                        self.initial_b @ self.items_means.T)[0, 1])
+
+        self.bs = defaultdict(lambda: self.initial_b.copy())
